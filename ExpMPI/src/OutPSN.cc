441eeac78e967abdf4b6c947931e39bdfa1862c2
weinberg <weinberg@localhost>
2002-05-25 23:58:00 +0000

    Phase space output with one file per dump

---
+++ ExpMPI/src/OutPSN.cc
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <strstream>
+
+#include "expand.h"
+#include <global.H>
+
+#include <AxisymmetricBasis.H>
+#include <OutPSN.H>
+
+OutPSN::OutPSN(string& line) : Output(line)
+{
+  initialize();
+}
+
+void OutPSN::initialize()
+{
+  string tmp;
+				// Get file name
+  if (!Output::get_value(string("filename"), filename)) {
+    filename.erase();
+    filename = "OUT\0";
+  }
+
+  if (Output::get_value(string("nint"), tmp))
+    nint = atoi(tmp.c_str());
+  else
+    nint = 100;
+
+  if (Output::get_value(string("nbeg"), tmp))
+    nbeg = atoi(tmp.c_str());
+  else
+    nbeg = 0;
+
+}
+
+
+void OutPSN::Run(int n, bool last)
+{
+  if (n % nint && !last) return;
+
+  ofstream *out;
+  list<Component*>::iterator cc;
+  Component* c;
+
+  if (myid==0) {
+				// Output name
+    ostrstream fname;
+    fname << filename << "." << nbeg++ << '\0';
+
+				// Open file and write master header
+    out = new ofstream(fname.str());
+
+    if (!*out) {
+      cerr << "OutPSN: can't open file <" << fname.str() 
+	   << "> . . . quitting\n";
+      MPI_Abort(MPI_COMM_WORLD, 33);
+    }
+				// Open file and write master header
+    
+    struct MasterHeader header;
+    header.time = tnow;
+    header.ntot = comp.ntot;
+    header.ncomp = comp.ncomp;
+
+    out->write(&header, sizeof(MasterHeader));
+  }
+  
+  for (cc=comp.components.begin(); cc != comp.components.end(); cc++) {
+    c = *cc;
+    c->write_binary(out);
+  }
+
+  if (myid==0) {
+    out->close();
+    delete out;
+  }
+
+}
+
