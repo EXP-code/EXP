72d1cfa64aca4490d3d3881f7ed0add954c117f3
weinberg <weinberg@localhost>
2002-06-05 18:42:41 +0000

    Coefficient output routines

---
+++ ExpMPI/src/OutCoef.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <strstream>
+
+#include <expand.h>
+
+#include <OutCoef.H>
+
+OutCoef::OutCoef(string& line) : Output(line)
+{
+  nint = 10;
+  filename = "outcoef";
+  tcomp = NULL;
+
+  initialize();
+
+  if (!tcomp) {
+    if (myid==0) {
+      cerr << "OutCoef: no component to trace\n";
+      MPI_Abort(MPI_COMM_WORLD, 112);
+    }
+  }
+
+  if (!(tcomp->force->HaveCoefDump())) {
+    if (myid==0) {
+      cerr << "OutCoef: no coefficients for this force\n";
+    }
+  }
+
+}
+
+void OutCoef::initialize()
+{
+  string tmp;
+				// Get file name
+  get_value(string("filename"), filename);
+  
+  if (get_value(string("nint"), tmp)) 
+    nint = atoi(tmp.c_str());
+
+				// Search for desired component
+  if (get_value(string("name"), tmp)) {
+    list<Component*>::iterator cc;
+    Component* c;
+    for (cc=comp.components.begin(); cc != comp.components.end(); cc++) {
+      c = *cc;
+      if (!(c->name.compare(tmp))) tcomp  = c;
+    }
+  }
+  
+}
+
+void OutCoef::Run(int n, bool last)
+{
+  if (n % nint && !last && !(tcomp->force->HaveCoefDump())) return;
+
+  MPI_Status status;
+
+				// Open output file
+  ofstream out;
+  if (myid==0) {
+    out.open(filename.c_str(), ios::out | ios::app);
+    if (!out) {
+      cout << "OutCoef: can't open file <" << filename << ">\n";
+    }
+    if (out) out << setw(15) << tnow;
+  }
+  
+  tcomp->force->dump_coefs(out);
+
+  out.close();
+}
