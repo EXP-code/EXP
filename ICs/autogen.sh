74b59295a6e5c3ed2f31498e4d8b247ab5bd2ba7
weinberg <weinberg@localhost>
2002-06-07 19:52:37 +0000

    For simplicity

---
+++ ICs/autogen.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if test ! -f install-sh ; then touch install-sh ; fi
+
+MAKE=`which gnumake`
+if test ! -x "$MAKE" ; then MAKE=`which gmake` ; fi
+if test ! -x "$MAKE" ; then MAKE=`which make` ; fi
+HAVE_GNU_MAKE=`$MAKE --version|grep -c "Free Software Foundation"`
+
+if test "$HAVE_GNU_MAKE" != "1"; then
+echo !!!! Warning: not tested with non Gnu-Make $MAKE
+else
+echo Found GNU Make at $MAKE ... good.
+fi
+
+echo This script runs configure and make...
+echo You did remember necessary arguments for configure, right?
+
+if test ! -x `which aclocal`
+then echo you need autoconfig and automake to generate the Makefile
+fi
+if test ! -x `which automake`
+then echo you need automake to generate the Makefile
+fi
+if test ! -x `which autoconf`
+then echo you need autoconf to generate the Makefile
+fi
+aclocal
+autoconf
+automake --add-missing
+automake --add-missing
+./configure $*
+$MAKE
