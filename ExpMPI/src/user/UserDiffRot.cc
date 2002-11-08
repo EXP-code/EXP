ddcb3d1c20c64e2c0c97709363447d0c510a31d1
weinberg <weinberg@localhost>
2002-11-08 15:35:56 +0000

    Added option to avoid particles.

--- ExpMPI/src/user/UserDiffRot.cc
+++ ExpMPI/src/user/UserDiffRot.cc
@@ -14,6 +14,9 @@ UserDiffRot::UserDiffRot(string &line) : ExternalForce(line)
 
   rate = 0.5;			// Rate relative to dyn time
   name = "";			// Default component name
+  avoid = "";
+  maxpm = 2;
+  first = true;
 
   initialize();
 
@@ -41,14 +44,45 @@ UserDiffRot::UserDiffRot(string &line) : ExternalForce(line)
   else
     c0 = NULL;
 
+  if (avoid.size()>0) {
+				// Look for component for particle
+				// avoidance
+    bool found = false;
+    list<Component*>::iterator cc;
+    Component *c;
+    for (cc=comp.components.begin(); cc != comp.components.end(); cc++) {
+      c = *cc;
+      if ( !avoid.compare(c->name) ) {
+	c1 = c;
+	found = true;
+      break;
+      }
+    }
+
+    if (!found) {
+      cerr << "Process " << myid << ": can't find desired component <"
+	   << avoid << ">" << endl;
+      c1 = NULL;
+    }
+
+  }
+  else
+    c1 = NULL;
+
+  pos = vector<double>(4*maxpm);
+  gen = new ACG(seed);
+  uniform = new Uniform(0.0, 1.0, gen);
+
   userinfo();
 }
+
 
 UserDiffRot::~UserDiffRot()
 {
   delete gen;
   delete uniform;
 }
+
 
 void UserDiffRot::userinfo()
 {
@@ -58,6 +92,8 @@ void UserDiffRot::userinfo()
   else
     cout << "** User routine ROTATION RANDOMIZATION disabled: no component specified";
   
+  cout << ", avoid = " << avoid;
+  cout << ", maxpm = " << maxpm;
   cout << ", rate = " << rate;
   cout << ", seed = " << seed;
   cout << endl;
@@ -71,11 +107,10 @@ void UserDiffRot::initialize()
   string val;
 
   if (get_value("name", val))		name = val;
+  if (get_value("avoid", val))		avoid = val;
+  if (get_value("maxpm", val))		maxpm = atoi(val.c_str());
   if (get_value("rate", val))		rate = atof(val.c_str());
   if (get_value("seed", val))		seed = atoi(val.c_str());
-
-  gen = new ACG(seed);
-  uniform = new Uniform(0.0, 1.0, gen);
 }
 
 
@@ -83,7 +118,72 @@ void UserDiffRot::determine_acceleration_and_potential(void)
 {
   if (!c0) return;
 
+  // Get particles to avoid
+  if (c1) {
+
+    ipm = 0;
+
+    if (myid==0) {
+
+      int number = -1;
+      Component::Partstruct *p = c1->get_particles(&number);
+
+      while (p) {
+	
+	for (int k=0; k<number && ipm<maxpm; k++, ipm++) {
+	  pos[ipm*4] = sqrt(p[k].pos[0]*p[k].pos[0] +
+			    p[k].pos[1]*p[k].pos[1] +
+			    p[k].pos[2]*p[k].pos[2] );
+	  pos[ipm*4+1] = p[k].pos[0];
+	  pos[ipm*4+2] = p[k].pos[1];
+	  pos[ipm*4+3] = p[k].pos[2];
+	}
+
+	if (ipm == maxpm) break;
+	p = c1->get_particles(&number);
+      }
+      
+      cout << "****************************************************************";
+      cout << "UserDiffRot: avoiding " << ipm << " particles\n";
+      cout << "****************************************************************";
+    }
+
+    MPI_Bcast(&ipm, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&pos[0], ipm*4, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  }
+
   exp_thread_fork(false);
+}
+
+
+double UserDiffRot::get_dtime(Particle& p) 
+{
+  double vv, E, Lx, Ly, Lz, dt;
+
+  // Compute energy
+  vv = 0.0;
+  for (int k=0; k<3; k++)
+    vv += p.vel[k] * p.vel[k];
+      
+  E = 0.5*vv + p.pot;
+
+  if (E>=0.0) E = -1.0e-10;
+  // Compute angular momentum
+  Lx = 
+    p.pos[1]*p.vel[2]-
+    p.pos[2]*p.vel[1];
+  
+  Ly = 
+    p.pos[2]*p.vel[0]-
+    p.pos[0]*p.vel[2];
+  
+  Lz =
+    p.pos[0]*p.vel[1]-
+    p.pos[1]*p.vel[0];
+  
+  dt = sqrt(Lx*Lx + Ly*Ly + Lz*Lz)/sqrt(-2.0*E);
+  
+  return dt;
 }
 
 
@@ -94,39 +194,44 @@ void * UserDiffRot::determine_acceleration_and_potential_thread(void * arg)
   int nbeg = 1+nbodies*id/nthrds;
   int nend = nbodies*(id+1)/nthrds;
 
-  double E, dt, phi, cosp, sinp;
-  double Lx, Ly, Lz, xx, yy, uu, vv;
+  double phi, cosp, sinp, diffr;
+  double xx, yy, uu, vv;
+
+  if (first) {
+    
+    indx = (*particles)[nbeg].dattrib.size();
+
+    for (int i=nbeg; i<nend; i++)
+      (*particles)[i].dattrib.push_back(tnow + 
+					get_dtime((*particles)[i])/rate);
+    
+    first = false;
+  }
+
 
   for (int i=nbeg; i<nend; i++) {
 
     if ((*particles)[i].freeze()) continue;
 
-				// Compute energy
-    vv = 0.0;
-    for (int k=0; k<3; k++)
-      vv += (*particles)[i].vel[k] * (*particles)[i].vel[k];
+				// Avoid?
+    bool tooclose = false;
+    for (int n=0; n<ipm; n++) {
+      diffr = 0.0;
+      for (int k=0; k<3; k++)
+	diffr += 
+	  ((*particles)[i].pos[k] - pos[n*4+1+k]) *
+	  ((*particles)[i].pos[k] - pos[n*4+1+k]) ;
 
-    E = 0.5*vv + (*particles)[i].pot;
-
-    if (E>=0.0) continue;
-
-				// Compute angular momentum
-    Lx = 
-      (*particles)[i].pos[1]*(*particles)[i].vel[2]-
-      (*particles)[i].pos[2]*(*particles)[i].vel[1];
-
-    Ly = 
-      (*particles)[i].pos[2]*(*particles)[i].vel[0]-
-      (*particles)[i].pos[0]*(*particles)[i].vel[2];
-
-    Lz =
-      (*particles)[i].pos[0]*(*particles)[i].vel[1]-
-      (*particles)[i].pos[1]*(*particles)[i].vel[0];
-
-    dt = sqrt(Lx*Lx + Ly*Ly + Lz*Lz)/sqrt(-2.0*E);
+      if (sqrt(diffr)<pos[n*4]) tooclose = true;
+    }
     
-				// Do the rotation
-    if ((*uniform)() < dtime*rate/dt) {
+    if (tooclose) continue;
+
+    if (tnow>(*particles)[i].dattrib[indx]) {
+
+      (*particles)[i].dattrib[indx] = tnow + get_dtime((*particles)[i])/rate;
+
+				// Do rotation
       phi = 2.0*M_PI*(*uniform)();
       cosp = cos(phi);
       sinp = sin(phi);
