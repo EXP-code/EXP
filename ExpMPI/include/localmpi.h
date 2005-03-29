38647fea2a7d6234afdd4ae85389bd7db1aced52
weinberg <weinberg@localhost>
2005-03-29 18:33:01 +0000

    Merging NewC branch onto HEAD at version 6.0

--- ExpMPI/include/localmpi.h
+++ ExpMPI/include/localmpi.h
@@ -1,20 +1,26 @@
+#ifndef _localmpi_h
+#define _localmpi_h
+
 #include <mpi.h>
 
 #ifdef USE_DMALLOC
 #include <dmalloc.h>
 #endif
 
-
-#ifdef IS_MAIN
-				/* MPI variables */
-int numprocs, slaves, myid, proc_namelen;
-char processor_name[MPI_MAX_PROCESSOR_NAME];
-
-#else
-				/* MPI variables */
-
 extern int numprocs, slaves, myid, proc_namelen;
 extern char processor_name[MPI_MAX_PROCESSOR_NAME];
 
-#endif /* IS_MAIN */
+#include <iostream>
+#include <iomanip>
+#include <string>
 
+//! For MPI debugging
+inline void mpi_report_location(const std::string& msg)
+{
+  for(int n=0; n<numprocs; n++) {
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (n==myid) std::cout << "Process " << myid << ": " << msg << std::endl;
+  }
+}
+
+#endif
