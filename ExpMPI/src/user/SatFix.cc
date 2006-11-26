a0bc8693249ef3a29d3c8491e273d8500a8c8266
weinberg <weinberg@localhost>
2006-11-26 19:57:29 +0000

    Debugged SatFix for a single satellite pair.

--- ExpMPI/src/user/SatFix.cc
+++ ExpMPI/src/user/SatFix.cc
@@ -49,7 +49,8 @@ void SatFix::userinfo()
   if (myid) return;		// Return if node master node
 
   print_divider();
-  cout << "Enforces mirror coordinates for adjacent particles\n";
+  cout << "** Enforces mirror coordinates for adjacent particles on component: " 
+       << c0->name << endl;
   print_divider();
 }
 
@@ -65,27 +66,85 @@ void SatFix::get_acceleration_and_potential(Component* C)
 {
   if (C != c0) return;
 
+  compute_list();
+
+  MPL_start_timer();
+
+				// Check for info from previous process
+  if (recv[myid]>=0) {
+    
+    MPI_Recv(C->Part(0)->pos, 3, MPI_DOUBLE, recv[myid], 133, 
+	     MPI_COMM_WORLD, &status);
+    MPI_Recv(C->Part(0)->vel, 3, MPI_DOUBLE, recv[myid], 134, 
+	     MPI_COMM_WORLD, &status);
+    MPI_Recv(C->Part(0)->acc, 3, MPI_DOUBLE, recv[myid], 135, 
+	     MPI_COMM_WORLD, &status);
+    
+				// Temporary!  Remove this . . .
+#if 0
+    print_recv();
+#endif
+				// Change sign of phase space
+    for (int k=0; k<3; k++) {
+      C->Part(0)->pos[k] *= -1.0;
+      C->Part(0)->vel[k] *= -1.0;
+      C->Part(0)->acc[k] *= -1.0;
+    }
+
+    check_recv();
+  }
+
+				// Send info to next process
+  if (send[myid]>=0) {
+    
+				// Temporary!  Remove this . . .
+#if 0
+    print_send();
+#endif
+
+    check_send();
+
+    MPI_Send(C->Part(end)->pos, 3, MPI_DOUBLE, send[myid], 133, 
+	     MPI_COMM_WORLD);
+    MPI_Send(C->Part(end)->vel, 3, MPI_DOUBLE, send[myid], 134, 
+	     MPI_COMM_WORLD);
+    MPI_Send(C->Part(end)->acc, 3, MPI_DOUBLE, send[myid], 135, 
+	     MPI_COMM_WORLD);
+  }
+
+  if (ncount[myid]) {
+    for (int n=begin; n<end; n+=2) {
+
+      check_body(n);
+
+      for (int k=0; k<3; k++) {
+	C->Part(n+1)->pos[k] = -C->Part(n)->pos[k];
+	C->Part(n+1)->vel[k] = -C->Part(n)->vel[k];
+	C->Part(n+1)->acc[k] = -C->Part(n)->acc[k];
+      }
+    } 
+  }
+
+  MPL_stop_timer();
+}
+
+void SatFix::compute_list()
+{
 				// Get body count
-  vector<int> ncount = c0->particle_count();
+  ncount = c0->particle_count();
+
 				// Check for change in body count
   bool recompute = false;
   for (int n=0; n<numprocs; n++) {
     if (last[n] != ncount[n]) recompute = true;
   }
   
-  if (verbose) 
-    cout << "Process " << myid << ": nbodies=" << ncount[myid] << endl;
-
-  if (!ncount[myid]) return;	// Do we have any particles?
-
-  MPI_Status status;
-  
   if (recompute) {
 				// Deal with end points
     send = vector<int>(numprocs, -1);
     recv = vector<int>(numprocs, -1);
     bool remainder = false;
-    int from, number;
+    int from=0, number;
 
     for (int n=0; n<numprocs; n++) {
       number = ncount[n];
@@ -106,24 +165,6 @@ void SatFix::get_acceleration_and_potential(Component* C)
 	
       }
     }
-				// Debug
-    if (verbose && myid==0) {
-
-      cout << "Node list" << endl 
-	   << "---------" << endl
-	   << setw(6) << "Node" 
-	   << setw(6) << "Send" 
-	   << setw(6) << "Recv" << endl;
-      
-      for (int n=0; n<numprocs; n++) {
-	cout << setw(6) << n;
-      if (send[n]<0) cout << setw(6) << "*";
-      else cout << setw(6) << send[n];
-      if (recv[n]<0) cout << setw(6) << "*";
-      else cout << setw(6) << recv[n];
-      cout << endl;
-      }
-    }
 
     begin = 0;
     end   = ncount[myid];
@@ -134,7 +175,47 @@ void SatFix::get_acceleration_and_potential(Component* C)
     last = ncount;
     recompute = false;
 
+				// Report particle allocation calculation
     if (verbose) {
+
+      if (myid==0) {
+
+	int begin1 = begin;
+	int end1   = end;
+
+	cout << "====================================================" << endl
+	     << "Component: " << c0->name << endl
+	     << "Node list for mirroring" << endl 
+	     << "-----------------------" << endl
+	     << setw(6) << "Node" 
+	     << setw(6) << "Count"
+	     << setw(6) << "Send" 
+	     << setw(6) << "Recv" 
+	     << setw(6) << "Begin" 
+	     << setw(6) << "End" 
+	     << endl;
+	
+	for (int n=0; n<numprocs; n++) {
+
+	  if (n) {
+	    MPI_Recv(&begin1, 1, MPI_INT, n, 131, MPI_COMM_WORLD, &status);
+	    MPI_Recv(&end1,   1, MPI_INT, n, 132, MPI_COMM_WORLD, &status);
+	  }
+
+	  cout << setw(6) << n << setw(6) << ncount[n];
+	  if (send[n]<0) cout << setw(6) << "*";
+	  else cout << setw(6) << send[n];
+	  if (recv[n]<0) cout << setw(6) << "*";
+	  else cout << setw(6) << recv[n];
+	  cout << setw(6) << begin1 << setw(6) << end1 << endl;
+	}
+
+	cout << "====================================================" << endl;
+	
+      } else {
+	MPI_Send(&begin, 1, MPI_INT, 0, 131, MPI_COMM_WORLD);
+	MPI_Send(&end,   1, MPI_INT, 0, 132, MPI_COMM_WORLD);
+      }
 				// Should always have an even number of 
 				// particles on each node after end points 
 				// are removed
@@ -144,53 +225,76 @@ void SatFix::get_acceleration_and_potential(Component* C)
     
   }
 
-  MPL_start_timer();
+}
 
-				// Check for info from previous process
-  if (recv[myid]>=0) {
-    
-    if (verbose) cout << "Process " << myid << ": receiving from Process " 
-		      << recv[myid] << endl;
-
-    MPI_Recv(C->Part(0)->pos, 3, MPI_DOUBLE, recv[myid], 133, 
-	     MPI_COMM_WORLD, &status);
-    MPI_Recv(C->Part(0)->vel, 3, MPI_DOUBLE, recv[myid], 134, 
-	     MPI_COMM_WORLD, &status);
-    MPI_Recv(C->Part(0)->acc, 3, MPI_DOUBLE, recv[myid], 135, 
-	     MPI_COMM_WORLD, &status);
-    
-				// Change sign of phase space
+void SatFix::check_send()
+{
+  if (verbose) {
+    bool ferror = false;
     for (int k=0; k<3; k++) {
-      C->Part(0)->pos[k] *= -1.0;
-      C->Part(0)->vel[k] *= -1.0;
-      C->Part(0)->acc[k] *= -1.0;
+      if (isnan(c0->Part(end)->pos[k])) ferror = true;
+      if (isnan(c0->Part(end)->vel[k])) ferror = true;
+      if (isnan(c0->Part(end)->acc[k])) ferror = true;
     }
-
+    if (ferror) {
+      cout << "Process " << myid << ": error in coordindates to be sent!" << endl;
+    }
   }
+  
+}
 
-				// Send info to next process
-  if (send[myid]>=0) {
-    
-    if (verbose) cout << "Process " << myid << ": sending to Process " 
-		      << send[myid] << endl;
-
-    MPI_Recv(C->Part(end)->pos, 3, MPI_DOUBLE, send[myid], 133, 
-	     MPI_COMM_WORLD, &status);
-    MPI_Recv(C->Part(end)->vel, 3, MPI_DOUBLE, send[myid], 134, 
-	     MPI_COMM_WORLD, &status);
-    MPI_Recv(C->Part(end)->acc, 3, MPI_DOUBLE, send[myid], 135, 
-	     MPI_COMM_WORLD, &status);
-
-  }
-
-  for (int n=begin; n<end; n+=2) {
+void SatFix::check_body(int n)
+{
+  if (verbose) {
+    bool ferror = false;
     for (int k=0; k<3; k++) {
-      C->Part(n+1)->pos[k] = -C->Part(n)->pos[k];
-      C->Part(n+1)->vel[k] = -C->Part(n)->vel[k];
-      C->Part(n+1)->acc[k] = -C->Part(n)->acc[k];
+      if (isnan(c0->Part(n)->pos[k])) ferror = true;
+      if (isnan(c0->Part(n)->vel[k])) ferror = true;
+      if (isnan(c0->Part(n)->acc[k])) ferror = true;
     }
-  } 
+    if (ferror) {
+      cout << "Process " << myid << ": error in coordindates, n=" << n << "!" << endl;
+    }
+  }
+}
 
-  MPL_stop_timer();
+void SatFix::check_recv()
+{
+  if (verbose) {
+    bool ferror = false;
+    for (int k=0; k<3; k++) {
+      if (isnan(c0->Part(0)->pos[k])) ferror = true;
+      if (isnan(c0->Part(0)->vel[k])) ferror = true;
+      if (isnan(c0->Part(0)->acc[k])) ferror = true;
+    }
+    if (ferror) {
+      cout << "Process " << myid << ": error in receiving coordindates!" << endl;
+    }
+  }
+}
+
+
+void SatFix::print_recv()
+{
+  if (verbose) {
+    cout << endl << "Process " << myid << ": received pos=";
+    for (int k=0; k<3; k++) cout << setw(16) << c0->Part(0)->pos[k];
+    cout << endl << "Process " << myid << ": received vel=";
+    for (int k=0; k<3; k++) cout << setw(16) << c0->Part(0)->vel[k];
+    cout << endl << "Process " << myid << ": received acc=";
+    for (int k=0; k<3; k++) cout << setw(16) << c0->Part(0)->acc[k];
+  }
+}
+
+void SatFix::print_send()
+{
+  if (verbose) {
+    cout << endl << "Process " << myid << ": sent pos=";
+    for (int k=0; k<3; k++) cout << setw(16) << c0->Part(end)->pos[k];
+    cout << endl << "Process " << myid << ": sent vel=";
+    for (int k=0; k<3; k++) cout << setw(16) << c0->Part(end)->vel[k];
+    cout << endl << "Process " << myid << ": sent acc=";
+    for (int k=0; k<3; k++) cout << setw(16) << c0->Part(end)->acc[k];
+  }
 }
 
