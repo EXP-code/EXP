1b91304ec32d77f563e4423fcc38c3c24bf47c08
weinberg <weinberg@localhost>
2008-08-11 03:49:08 +0000

    Mistake in linear interpolation (typo of course).

--- ExpMPI/utils/ICs/bonnerebert.cc
+++ ExpMPI/utils/ICs/bonnerebert.cc
@@ -64,14 +64,13 @@ string oname;			/* --output */
 static struct option const long_options[] =
 {
   {"output",  required_argument, 0,    'o'},
-  {"xfinal",  required_argument, 0,    'x'},
   {"xstep",   required_argument, 0,    'd'},
   {"ratio",   required_argument, 0,    'r'},
+  {"munit",   required_argument, 0,    'M'},
   {"mass",    required_argument, 0,    'm'},
   {"temp",    required_argument, 0,    'T'},
   {"runit",   required_argument, 0,    'R'},
   {"number",  required_argument, 0,    'N'},
-  {"msun",    required_argument, 0,    'M'},
   {"help",    no_argument,       0,    'h'},
   {"version", no_argument,       0,    'V'},
   {NULL,      0,                 NULL,  0 }
@@ -82,10 +81,10 @@ static int decode_switches (int argc, char **argv);
 
 double X = 1000.0;
 double h = 0.01;
-double Nmsun = 1.0e12;		// Number of solar masses per unit
-double M = 1.0;			// units of 10^12 (Nmsun) solar masses
+double Munit = 1e12;		// Unit of mass in solar units
+double M = 1.0;			// number of units for halo
 double ratio = 14.0;		// rho_c/rho_t
-double T = 10000.0;		// degrees kelvin
+double T = 1000000.0;		// degrees kelvin
 double R = 300.0;		// Unit dimension in kpc
 unsigned N = 0;			// Number of particles
 unsigned S = 11;		// random # seed
@@ -95,18 +94,23 @@ void deriv(double x, vector<double>&y, vector<double>&a)
   /*
    y_1 = \Psi
    y_2 = d\Psi/dx = dy_1/dx
+   y_3 = m(x)
 
    f_1 = y_2
    f_2 = exp(-y_1) - 2y_2/x
+   f_3 = 4*pi*x*x*exp(-\Psi)
   */
+  double rho = exp(-y[0]);
 
   a[0] = y[1];
   if (x>0.0) {
     a[0] = y[1];
-    a[1] = exp(-y[0]) - 2.0*y[1]/x;
+    a[1] = rho - 2.0*y[1]/x;
+    a[2] = 4.0*M_PI*x*x*rho;
   } else {
     a[0] = 0.0;
     a[1] = 1.0;
+    a[2] = 0.0;
   }
 }
 
@@ -134,14 +138,15 @@ vector< vector<double> > solution;
 void accum(double& x, vector<double>& y)
 {
   double rho = exp(-y[0]), xxy=x*x*y[1];
-  vector<double> record(7);
+  vector<double> record(8);
   record[0] = x;
   record[1] = y[0];
   record[2] = y[1];
-  record[3] = xxy;
-  record[4] = rho;
-  record[5] = sqrt(0.25*rho/M_PI)*xxy;
-  record[6] = 1.0/record[4];
+  record[3] = y[2];
+  record[4] = xxy;
+  record[5] = rho;
+  record[6] = sqrt(0.25*rho/M_PI)*xxy;
+  record[7] = 1.0/record[5];
 
   solution.push_back(record);
 }
@@ -165,19 +170,27 @@ int vlocate(double x, unsigned j, const vector< vector<double> > &xx)
   return lo;
 }
 
-double linear(double x, unsigned j, const vector< vector<double> >& tab)
+double linear(double x, unsigned i, unsigned j, const vector< vector<double> >& tab)
 {
   int imin = 0;
   int imax = tab.size() - 1;
 
-  int index = vlocate(x, 0, tab);
+  int index = vlocate(x, i, tab);
 
   if (index <  imin) index = imin;
   if (index >= imax) index = imax-1;
 
-  return ( tab[j][index+1]*(x-tab[0][index]  ) -
-	   tab[j][index  ]*(x-tab[0][index+1]) )
-    /( tab[0][index+1]-tab[0][index] ) ;
+  double denom = tab[index+1][i]-tab[index][i];
+
+  double result = 
+    ( tab[index+1][j]*(x-tab[index  ][i]  )   -
+      tab[index  ][j]*(x-tab[index+1][i]) ) /denom;
+
+  if (isinf(result) || isnan(result)) {
+    cerr << "oops" << endl;
+  }
+
+  return result;
 }
 
 int
@@ -212,11 +225,12 @@ main (int argc, char **argv)
   // Initial condtions
   //
 
-  vector<double> y(2), a(2);
+  vector<double> y(3), a(3);
 
   double x = 0.0;
   y[0] = 0.0;
   y[1] = 0.0;
+  y[2] = 0.0;
 
   accum(x, y);
 
@@ -235,19 +249,19 @@ main (int argc, char **argv)
 
   int n;
 
-  if (ratio>=solution.back()[6])
+  if (ratio>=solution.back()[7])
     n = solution.size()-2;
-  else if (ratio<=solution.front()[6]) 
+  else if (ratio<=solution.front()[7]) 
     n = 0;
   else 
-    n = vlocate(ratio, 6, solution);
+    n = vlocate(ratio, 7, solution);
 
-  double denom = solution[n+1][6] - solution[n][6];
-  double A = (solution[n+1][6] - ratio)/denom;
-  double B = (ratio - solution[n][6])/denom;
+  double denom = solution[n+1][7] - solution[n][7];
+  double A = (solution[n+1][7] - ratio)/denom;
+  double B = (ratio - solution[n][7])/denom;
 
   double xt = A*solution[n][0] + B*solution[n+1][0];
-  double mt = A*solution[n][5] + B*solution[n+1][5];
+  double mt = A*solution[n][6] + B*solution[n+1][6];
 
   //
   // Units
@@ -263,34 +277,40 @@ main (int argc, char **argv)
   double mm = f_H*mp + (1.0-f_H)*4.0*mp;
   double cs2 = boltz*T/mm;
 
-  double Pt = mt*cs2*cs2/(pow(G, 1.5)*M*Nmsun*msun); Pt *= Pt;
+  double Pt = mt*cs2*cs2/(pow(G, 1.5)*M*Munit*msun); Pt *= Pt;
   double Rhot = Pt/cs2;
   double Rhoc = Rhot*ratio;
 
-				// in kiloparsec
+				// in units of halo
   double rfac   = sqrt(cs2/(4.0*M_PI*G*Rhoc))/(R*1.0e3*pc);
-				// solar masses/kpc^3
-  double rhofac = Rhoc*pow(1.0e3*pc, 3.0)/msun;
-				// in units of 10^10 solar masses
-  double mfac   = cs2*cs2/(sqrt(Pt)*pow(G, 1.5))/(Nmsun*msun);
+
+				// units of Munit/(halo size)^3
+  double rhofac = Rhoc*pow(R*1.0e3*pc, 3.0)/(Munit*msun);
+
+				// in units of Munit solar masses
+  double mfac   = cs2*cs2/(sqrt(Pt)*pow(G, 1.5))/(Munit*msun);
+
+  double mfac2  = rfac*rfac*rfac*rhofac;
 
   if (N==0) {
 
     for (unsigned i=0; i<=n; i++) {
       fout << setw(15) << solution[i][0]*rfac;
-      fout << setw(15) << solution[i][4]*rhofac;
-      fout << setw(15) << solution[i][5]*mfac;
+      fout << setw(15) << solution[i][5]*rhofac;
+      fout << setw(15) << solution[i][6]*mfac;
+      fout << setw(15) << solution[i][3]*mfac2;
       fout << endl;
     }
     fout << setw(15) << (A*solution[n][0] + B*solution[n+1][0])*rfac;
-    fout << setw(15) << (A*solution[n][4] + B*solution[n+1][4])*rhofac;
-    fout << setw(15) << (A*solution[n][5] + B*solution[n+1][5])*mfac;
+    fout << setw(15) << (A*solution[n][5] + B*solution[n+1][5])*rhofac;
+    fout << setw(15) << (A*solution[n][6] + B*solution[n+1][6])*mfac;
+    fout << setw(15) << (A*solution[n][3] + B*solution[n+1][3])*mfac2;
     fout << endl;
 
   } else {
 
 				// Circ velocity unit at edge in cgs
-    double vcirc = sqrt(G*M*Nmsun*msun/(R*1e3*pc));
+    double vcirc = sqrt(G*M*1.0e10*msun/(R*1e3*pc));
     double vfac = sqrt(cs2)/vcirc;
 
     ACG gen(S);
@@ -298,29 +318,19 @@ main (int argc, char **argv)
     Normal  norm(0.0, 1.0, &gen);
     vector<double> pos(3);
     double mass = M/N, x, phi, cost, sint, m;
-    const int ITMAX = 1000;
-    const double MMAX = 1.2;
+    double MMAX = linear(xt, 0, 3, solution);
 
     fout << " " << N << " 0 4" << endl; // PSP header
     fout.precision(10);
 
     for (int i=0; i<N; i++) {
-
-				// Evaluate by rejection so we can
-				// do unstable spheres later
-      unsigned j;
-      for (j=0; j<ITMAX; j++) {
-	x = xt*unit();
-	m = linear(x, 5, solution);
-	if (unit() < m/MMAX) break;
-      }
-      if (j==ITMAX) { cerr << "Oops" << endl; }
-
-      x *= rfac;
+      m = MMAX*unit();
+      x = linear(m, 3, 0, solution) * rfac;
 
       phi  = 2.0*M_PI*unit();
       cost = 2.0*unit() - 1.0;
       sint = sqrt(1.0 - cost*cost);
+
       pos[0] = x*sint*cos(phi);
       pos[1] = x*sint*sin(phi);
       pos[2] = x*cost;
@@ -347,14 +357,13 @@ decode_switches (int argc, char **argv)
 
   while ((c = getopt_long (argc, argv, 
 			   "o:"	/* output  */
-			   "x:"	/* xfinal  */
 			   "d:"	/* xstep   */
 			   "r:"	/* ratio   */
+			   "M:"	/* munit   */
 			   "m:"	/* mass    */
 			   "T:"	/* temp    */
 			   "R:"	/* runit   */
 			   "N:"	/* number  */
-			   "M:"	/* msun  */
 			   "S:"	/* seed    */
 			   "h"	/* help    */
 			   "V",	/* version */
@@ -365,9 +374,6 @@ decode_switches (int argc, char **argv)
 	case 'o':		/* --output */
 	  oname = string(optarg);
 	  break;
-	case 'x':		/* --xfinal */
-	  X = atof(optarg);
-	  break;
 	case 'd':		/* --xstep */
 	  h = atof(optarg);
 	  break;
@@ -377,11 +383,11 @@ decode_switches (int argc, char **argv)
 	case 'R':		/* --runit */
 	  R = atof(optarg);
 	  break;
+	case 'M':		/* --munit */
+	  Munit = atof(optarg);
+	  break;
 	case 'm':		/* --mass */
 	  M = atof(optarg);
-	  break;
-	case 'M':		/* --msun */
-	  Nmsun = atof(optarg);
 	  break;
 	case 'T':		/* --temp */
 	  T = atof(optarg);
@@ -414,9 +420,15 @@ usage (int status)
   cout << program_name << " - Integrate Lane-Emden equation using RK4" << endl;
   cout << "Usage: " << program_name << " [OPTION]... [FILE]..." << endl;
   cout << "Options:" << endl
-       << "  -o, --output NAME          send output to NAME instead of standard output" << endl
-       << "  --verbose                  print more information" << endl
-       << "  -h, --help                 display this help and exit" << endl
-       << "  -V, --version              output version information and exit" << endl;
+       << "  -o, --output STRING      send output to NAME" << endl
+       << "  -d, --xstep  FLOAT       ODE step in dimensionless units" << endl
+       << "  -r, --ratio  FLOAT       central to edge density ratio" << endl
+       << "  -M, --msun   FLOAT       mass unit in M_sun (default: 10^12)" << endl
+       << "  -m, --mass   FLOAT       mass of sphere in mass units" << endl
+       << "  -T, --temp   FLOAT       temperature in degress K" << endl
+       << "  -R, --runit  FLOAT       radial unit in kpc" << endl
+       << "  -N, --number INT         number of particles (0 means print profile)" << endl
+       << "  -h, --help               display this help and exit" << endl
+       << "  -V, --versio             output version information and exit" << endl;
   exit (status);
 }
