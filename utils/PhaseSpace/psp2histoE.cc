a1955ed5d6edffe5c4f3d89132abbb4eb15296f4
Martin D. Weinberg <weinberg@astro.umass.edu>
2015-05-22 14:28:06 -0400

    Generalized psp2histoE to compute ion and electron temperatures.  Added plotting scripts to Analyzie for use with psp2histoE.

--- utils/PhaseSpace/psp2histoE.cc
+++ utils/PhaseSpace/psp2histoE.cc
@@ -63,7 +63,7 @@ main(int ac, char **av)
      "number of bins")
     ("species,s",	po::value<int>(&sindx)->default_value(0),
      "position of species index")
-    ("electrons,e",	po::value<int>(&eindx)->default_value(6),
+    ("electrons,e",	po::value<int>(&eindx)->default_value(7),
      "position of electron index")
     ("name,c",	        po::value<std::string>(&cname)->default_value("gas"),
      "component name")
@@ -86,7 +86,7 @@ main(int ac, char **av)
     std::cout << desc << std::endl;
     std::cout << "Example: " << std::endl;
     std::cout << "\t" << av[0]
-	      << " --temp=25000 --number=250000 --output=out.bod" << std::endl;
+	      << " -E 300 -n 100 -f OUT.run.00001" << std::endl;
     return 1;
   }
 
@@ -96,10 +96,42 @@ main(int ac, char **av)
 
   // Units
   //
-  const double m_e = 9.10938215e-28; // electron mass in g
+  const double amu = 1.66053892e-24; // atomic mass unit in g
   const double eV  = 1.60217653e-12; // erg per eV
   double Vunit     = Lunit/Tunit;
-  double factor    = m_e/eV * Vunit*Vunit;
+  double KEfac     = 0.5 * amu/eV * Vunit*Vunit;
+
+  const std::vector<double> atomic_mass = {0.000549,  // 0  electron
+					   1.00797,   // 1  H
+					   4.00260,   // 2  He
+					   6.941,     // 3  Li
+					   9.01218,   // 4  Be
+					   10.81,     // 5  B
+					   12.011,    // 6  C
+					   14.0067,   // 7  N
+					   15.9994,   // 8  O
+					   18.998403, // 9  F
+					   20.179,    // 10 Ne
+					   22.98977,  // 11 Na
+					   24.305,    // 12 Mg
+					   26.98154,  // 13 Al
+					   28.0855,   // 14 Si
+					   30.97376,  // 15 P
+					   32.06,     // 16 S
+					   35.453,    // 17 Cl
+					   39.948,    // 18 Ar
+					   39.0983,   // 19 K
+					   40.08,     // 20 Ca
+					   44.9559,   // 21 Sc
+					   47.90,     // 22 Ti
+					   50.9415,   // 23 V
+					   51.996,    // 24 Cr
+					   54.9380,   // 25 Mn
+					   55.847,    // 26 Fe
+					   58.9332,   // 27 Co
+					   58.70,     // 28 Ni
+					   63.546,    // 29 Cu
+					   65.38 };   // 30 Zn
 
   // Get file arguments
   //
@@ -149,9 +181,16 @@ main(int ac, char **av)
 
 				// Will contain array for each species
 				//
-    std::map< speciesKey, std::vector<float> > shist;
+
+    typedef std::pair< std::vector<float>, std::vector<float> > pVec;
+    const std::vector<float> zero(numb+1, 0.0);
+    const pVec pZero(zero, zero);
+
+    std::map< speciesKey, pVec> shist;
+    pVec thist(pZero);
+
     double dkE   = Emax/numb;
-    double total = 0.0;
+    double etotal = 0.0, itotal = 0.0;
 
     for (stanza=psp.GetStanza(); stanza!=0; stanza=psp.NextStanza()) {
     
@@ -164,23 +203,33 @@ main(int ac, char **av)
 
       for (part=psp.GetParticle(in); part!=0; part=psp.NextParticle(in)) {
 
-	double kE = 0.0;
+	double kEe = 0.0, kEi = 0.0;
 	for (size_t i=0; i<3; i++) {
-	  double v = part->datr(eindx+i);
-	  kE += v*v;
+	  double ve = part->datr(eindx+i);
+	  kEe += ve*ve;
+	  double vi = part->vel(i);
+	  kEi += vi*vi;
 	}
-	kE *= factor;
-
 	KeyConvert kc(part->iatr(sindx));
+
+	kEe *= KEfac * atomic_mass[0];
+	kEi *= KEfac * atomic_mass[kc.Z()];
+
 	speciesKey k = kc.getKey();
-
-	if (shist.find(k) == shist.end()) shist[k].resize(numb+1, 0.0);
+	if (shist.find(k) == shist.end()) shist[k] = pZero;
       
-	size_t l = std::min<size_t>(kE/dkE, numb);
+	size_t le   = std::min<size_t>(kEe/dkE, numb);
+	size_t li   = std::min<size_t>(kEi/dkE, numb);
+	double wgte = part->mass() * (kc.C() - 1);
+	double wgti = part->mass() ;
 
-	double wgt = part->mass() * (k.second - 1);
-	shist[k][l] += wgt;
-	total += wgt;
+	shist[k].first[le]  += wgte;
+	thist   .first[le]  += wgte;
+	etotal              += wgte;
+
+	shist[k].second[li] += wgti;
+	thist   .second[li] += wgti;
+	itotal              += wgti;
       }
     }
     
@@ -197,19 +246,22 @@ main(int ac, char **av)
       
       for (auto v : shist) {
 	speciesKey k = v.first;
-	ostringstream str;
-	str << "(" << k.first << ", " << k.second << ")";
-	cout << setw(fw) << str.str();
+	ostringstream stre, stri;
+	stre << "(" << k.first << "," << k.second << ")_e";
+	stri << "(" << k.first << "," << k.second << ")_i";
+	cout << setw(fw) << stre.str() << setw(fw) << stri.str();
       }
-      cout << std::endl;
+      cout << setw(fw) << "Total_e" << setw(fw) << "Total_i" << std::endl;
 
       std::cout << setw(fw) << std::string(sw, '-')
 		<< setw(fw) << std::string(sw, '-');
 
       for (auto v : shist) {
-	cout << setw(fw) << std::string(sw, '-');
+	cout << setw(fw) << std::string(sw, '-')
+	     << setw(fw) << std::string(sw, '-');
       }
-      cout << std::endl;
+      cout << setw(fw) << std::string(sw, '-')
+	   << setw(fw) << std::string(sw, '-') << std::endl;
 
       first = false;
     }
@@ -219,18 +271,26 @@ main(int ac, char **av)
       cout << setw(fw) << Time 
 	   << setw(fw) << energy;
       for (auto v : shist) {
-	double z = v.second[i];
-	cout << setw(fw) << z/total;
+	double ze = v.second.first [i];
+	double zi = v.second.second[i];
+	cout << setw(fw) << ze/etotal
+	     << setw(fw) << zi/itotal;
       }
-      cout << endl;
+      cout << setw(fw) << thist.first [i]/etotal 
+	   << setw(fw) << thist.second[i]/itotal 
+	   << endl;
     }
     cout << setw(fw) << Time 
 	 << setw(fw) << "Overflow";
     for (auto v : shist) {
-      double z = v.second[numb];
-      cout << setw(fw) << z/total;
+      double ze = v.second.first [numb];
+      double zi = v.second.second[numb];
+      cout << setw(fw) << ze/etotal
+	   << setw(fw) << zi/itotal;
     }
-    cout << endl << endl;
+    cout << setw(fw) << thist.first [numb]/etotal 
+	 << setw(fw) << thist.second[numb]/itotal 
+	 << endl << endl;
   }
 
   return 0;
