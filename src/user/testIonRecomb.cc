db620bb42745037b34449c053b9af4db1c8ddec1
Martin D. Weinberg <weinberg@astro.umass.edu>
2016-04-02 12:55:12 -0400

    Added native ionization and recombination rate coefficients

--- src/user/testIonRecomb.cc
+++ src/user/testIonRecomb.cc
@@ -11,35 +11,11 @@
 
 #include <boost/program_options.hpp>
 
-#include <gaussQ.h>
-
-#include "atomic_constants.H"
 #include "Ion.H"
-#include "Elastic.H"
 
 namespace po = boost::program_options;
 
 #include <mpi.h>
-
-// For sorting tuples
-//
-template<int M, template<typename> class F = std::less>
-struct TupleCompare
-{
-  template<typename T>
-  bool operator()(T const &t1, T const &t2)
-  {
-    return F<typename std::tuple_element<M, T>::type>()(std::get<M>(t1), std::get<M>(t2));
-  }
-};
-
-double df(double v, double T)
-{
-  const double b = 0.5*me/boltz;
-  const double c = 4.0/sqrt(M_PI)*pow(b, 1.5);
-  double v2 = v*v;
-  return c * pow(T, -1.5) * v2 * exp(-b*v2);
-}
 
 // Global variables (not all used but needed for EXP libraries)
 
@@ -69,12 +45,10 @@ int main (int ac, char **av)
   double Tmin, Tmax;
   std::string RRtype;
   int numT, norder;
-  bool rates;
 
   po::options_description desc("Allowed options");
   desc.add_options()
     ("help,h",		"produce help message")
-    ("rates,r",         "print rate coefficients")
     ("Z,Z",		po::value<unsigned short>(&Z)->default_value(2),
      "atomic number")
     ("Tmin,t",		po::value<double>(&Tmin)->default_value(1000.0),
@@ -102,14 +76,11 @@ int main (int ac, char **av)
     return -1;
   }
 
-  if (vm.count("rates")) rates = true;
-  else                   rates = false;
-
   if (vm.count("help")) {
     std::cout << desc << std::endl;
-    std::cout << "Example: Helium II recombination" << std::endl;
+    std::cout << "Example: Helium ionization fractions at T=40,000 K" << std::endl;
     std::cout << "\t" << av[0]
-	      << " -Z 2 -C 2" << std::endl;
+	      << " -Z 2 -T 40000" << std::endl;
     MPI_Finalize();
     return 1;
   }
@@ -165,61 +136,14 @@ int main (int ac, char **av)
   double tmax = log(Tmax);
   double dT   = (tmax - tmin)/numT;
 
-  std::map<lQ, double> ionize, recomb;
-
-  for (unsigned short C=1; C<=Z+0; C++) ionize[lQ(Z, C)] = 0.0;
-  for (unsigned short C=2; C<=Z+1; C++) recomb[lQ(Z, C)] = 0.0;
-
-  LaguQuad lq(norder);
-
-  const double beta = 0.5*me/boltz;
-
   for (int nt=0; nt<=numT; nt++) {
 
     double T = exp(tmin + dT*nt);
 
-    for (auto & v : ionize) v.second = 0.0;
-    for (auto & v : recomb) v.second = 0.0;
-
-    for (int i=1; i<=norder; i++) {
-      double y = lq.knot(i);
-      double w = lq.weight(i);
-
-      // Ionize
-      for (auto & v : ionize) {
-	double ionE = ch.ipdata[v.first];  // ionization energy in eV
-	double ab   = ionE/(boltzEv*T);	  
-	double eab  = exp(-ab);	           // Boltzmann factor for ionization
-	double EeV  = (y + ab)*boltzEv*T;  // eval energy
-	double vel  = sqrt((y + ab)/beta); // eval velocity squared
-
-	v.second += w * eab * vel *
-	  ch.IonList[v.first]->directIonCross(EeV, 0);
-      }
-
-      // Recomb
-      for (auto & v : recomb) {
-	double EeV  = y*boltzEv*T;
-	double vel  = sqrt(y/beta);
-
-	v.second += w * vel *
-	  ch.IonList[v.first]->radRecombCross(EeV, 0).back();
-      }
-    }
-
+    std::map<unsigned short, double> values = ch.fraction(Z, T, norder);
+    
     out << std::setw(16) << T;
-    if (rates) {
-      for (auto & v : ionize) out << std::setw(16) << v.second;
-      for (auto & v : recomb) out << std::setw(16) << v.second;
-    }
-
-    std::vector<double> nn(Z+1, 1);
-    double norm = 1.0;
-    for (unsigned short C=1; C<=Z; C++) {
-      nn[C] = nn[C-1] * ionize[lQ(Z,C)]/recomb[lQ(Z,C+1)];
-      norm += nn[C];
-    }
-    for (auto n : nn) out << std::setw(16) << n/norm;
+    for (auto v : values) out << std::setw(16) << v.second;
     out << std::endl;
   }
 
