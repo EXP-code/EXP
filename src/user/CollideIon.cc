4db54ac59ba8e7556aa74e328776fe0b54e25965
Martin D. Weinberg <weinberg@astro.umass.edu>
2016-05-10 17:34:21 -0400

    Fixed typo

--- src/user/CollideIon.cc
+++ src/user/CollideIon.cc
@@ -10193,7 +10193,7 @@ Collide::Interact CollideIon::generateSelectionHybridSub
   double ke = std::max<double>(0.5*me*eVel*eVel/eV, FloorEv);
 
 				// Cache assigned energy
-  kE1[id] = kE2[id] = ke;
+  kEe1[id] = kEe2[id] = ke;
 
   // Loop through ionization levels in p1.  p2 only donates an
   // electron velocity and its weight does not matter.
