a5da43ca48e874bfcc5e1fb52ec40f21eb9d69c3
Martin D. Weinberg <weinberg@astro.umass.edu>
2017-06-19 15:01:52 -0400

    White space clean up

--- src/user/NTC.cc
+++ src/user/NTC.cc
@@ -87,212 +87,212 @@ std::ostream& operator<< (std::ostream& out, const sKeyPair& t)
   return out;
 }
 
- void NTCitem::Test()
- {
-   inTest = true;		// To prevent recursive calling in Prob
+void NTCitem::Test()
+{
+  inTest = true;		// To prevent recursive calling in Prob
+  
+  // Size of sample and separator bar
+  const size_t number = 10;
+  size_t wid = number * 12 + 10 + 14;
+  
+  if (db.size()==0)
+    std::cout << "Empty caller: " << caller << " : " 
+	      << " p: "  << &db
+	      << std::endl;
+  
+  if (db.size()>0) {
+    
+    std::cout << std::string(70, '-') << std::endl
+	      << "VelCrs structure: "  << this << " caller: " << caller 
+	      << std::endl << std::string(70, '-') << std::endl;
+    
+    for (auto v : db) {
+      sKeyPair p = v.first;
+      std::ostringstream sout;
+      sout << "<" << p.first.first  << "," << p.first.second
+	   << "|" << p.second.first << "," << p.second.second << ">";
+      
+      size_t prc = std::cout.precision(2);
+      
+      for (auto k : v.second) {
+	
+	std::cout << std::setw(14) << sout.str()
+		  << std::setw(10) << k.first
+		  << std::setw(10) << k.second.count();
+	
+	for (size_t i=0; i<number; i++) {
+	  double P = (0.5 + i)/number;
+	  std::cout << std::setw(10) << k.second(P);
+	}
+	std::cout << std::endl;
+      }
+      std::cout.precision(prc);
+    }
+    std::cout << std::string(wid, '-') << std::endl;
+  } else {
+    std::cout << std::string(wid, '-') << std::endl
+	      << "VelCrs no points: " << this << std::endl
+	      << std::string(wid, '-') << std::endl;
+  }
+  
+  inTest = false;
+}
 
-				 // Size of sample and separator bar
-   const size_t number = 10;
-   size_t wid = number * 12 + 10 + 14;
+NTCitem::vcMap NTCitem::Prob(double x)
+{
+  vcMap ret;
+  
+  // Deal with round off issues on resume
+  //
+  for (auto v : db) {
+    for (auto u : v.second)
+      ret[v.first][u.first] = u.second.inverse(x);
+  }
+  return ret;
+}
 
-   if (db.size()==0)
-     std::cout << "Empty caller: " << caller << " : " 
-	       << " p: "  << &db
-	       << std::endl;
+double NTCitem::Prob(sKeyPair indx, const T& intr, double x)
+{
+  oddBall(indx, intr);
+  
+  // Get stanza in db
+  qpMap::iterator jt = db.find(indx);
+  
+  // Default value
+  if (jt == db.end()) return 0.5;
+  
+  // Get next stanza 
+  uqMap::iterator it = jt->second.find(intr);
+  
+  // Default value
+  if (it == jt->second.end()) return 0.5;
+  
+  // Debug reporting
+  //
+  if (DEBUG_V and !inTest) {
+    report++;			// Make sure the first one is skipped
+    if (report % skprpt == 0 and report < maxrpt*skprpt)  Test();
+  }
+  
+  // Return quantile
+  return it->second.inverse(x);
+}
 
-   if (db.size()>0) {
+void NTCitem::debug()
+{
+  std::cout << std::string(94, '-') << std::endl;
+  std::cout << std::left << std::setw(30) << "Species pair"
+	    << std::left << std::setw( 6) << "Inter"
+	    << std::left << std::setw(10) << "Count"
+	    << std::left << std::setw(18) << "Min value"
+	    << std::left << std::setw(18) << "Max value"
+	    << std::left << std::setw(18) << "Quantile"
+	    << std::endl;
+  
+  for (auto u : db) {
+    for (auto v : u.second) {
+      std::cout << std::left << std::setw(30) << u.first 
+		<< std::left << std::setw(10) << v.first
+		<< std::left << std::setw(10) << v.second.count() 
+		<< std::left << std::setw(18) << v.second.xmin()
+		<< std::left << std::setw(18) << v.second.xmax()
+		<< std::left << std::setw(18) << v.second(0.95)
+		<< std::endl;
+      v.second.dump(std::cout);
+    }    
+  }
+  std::cout << std::string(94, '-') << std::endl;
+}
 
-     std::cout << std::string(70, '-') << std::endl
-	       << "VelCrs structure: "  << this << " caller: " << caller 
-	       << std::endl << std::string(70, '-') << std::endl;
+double NTCitem::CrsVel(sKeyPair indx, const T& intr, double p)
+{
+  oddBall(indx, intr);
+  
+  // Get stanza in db
+  qpMap::iterator it = db.find(indx);
+  
+  // Default value
+  if (it == db.end()) return Def;
+  
+  // Get next stanza
+  uqMap::iterator jt = it->second.find(intr);
+  
+  // Default value
+  if (jt == it->second.end()) return Def;
+  
+  
+  // Default value
+  if (! jt->second.full() ) return Def;
+  
+  if (0) debug();
+  
+  // Return value
+  return jt->second(p);
+}
 
-     for (auto v : db) {
-       sKeyPair p = v.first;
-       std::ostringstream sout;
-       sout << "<" << p.first.first  << "," << p.first.second
-	    << "|" << p.second.first << "," << p.second.second << ">";
+NTCitem::vcMap NTCitem::CrsVel(double p)
+{
+  vcMap ret;
+  
+  for (auto v : db) {
+    for (auto u : v.second)
+      ret[v.first][u.first] = u.second(p);
+  }
+  
+  // Return value
+  return ret;
+}
 
-       size_t prc = std::cout.precision(2);
-
-       for (auto k : v.second) {
-
-	 std::cout << std::setw(14) << sout.str()
-		   << std::setw(10) << k.first
-		   << std::setw(10) << k.second.count();
-
-	 for (size_t i=0; i<number; i++) {
-	   double P = (0.5 + i)/number;
-	   std::cout << std::setw(10) << k.second(P);
-	 }
-	 std::cout << std::endl;
-       }
-       std::cout.precision(prc);
-     }
-     std::cout << std::string(wid, '-') << std::endl;
-   } else {
-     std::cout << std::string(wid, '-') << std::endl
-	       << "VelCrs no points: " << this << std::endl
-	       << std::string(wid, '-') << std::endl;
-   }
-
-   inTest = false;
- }
-
- NTCitem::vcMap NTCitem::Prob(double x)
- {
-   vcMap ret;
-
-   // Deal with round off issues on resume
-   //
-   for (auto v : db) {
-     for (auto u : v.second)
-       ret[v.first][u.first] = u.second.inverse(x);
-   }
-   return ret;
- }
-
- double NTCitem::Prob(sKeyPair indx, const T& intr, double x)
- {
-   oddBall(indx, intr);
-
-   // Get stanza in db
-   qpMap::iterator jt = db.find(indx);
-
-   // Default value
-   if (jt == db.end()) return 0.5;
-
-   // Get next stanza 
-   uqMap::iterator it = jt->second.find(intr);
-
-   // Default value
-   if (it == jt->second.end()) return 0.5;
-
-   // Debug reporting
-   //
-   if (DEBUG_V and !inTest) {
-     report++;			// Make sure the first one is skipped
-     if (report % skprpt == 0 and report < maxrpt*skprpt)  Test();
-   }
-
-   // Return quantile
-   return it->second.inverse(x);
- }
-
- void NTCitem::debug()
- {
-   std::cout << std::string(94, '-') << std::endl;
-   std::cout << std::left << std::setw(30) << "Species pair"
-	     << std::left << std::setw( 6) << "Inter"
-	     << std::left << std::setw(10) << "Count"
-	     << std::left << std::setw(18) << "Min value"
-	     << std::left << std::setw(18) << "Max value"
-	     << std::left << std::setw(18) << "Quantile"
-	     << std::endl;
-
-   for (auto u : db) {
-     for (auto v : u.second) {
-       std::cout << std::left << std::setw(30) << u.first 
-		 << std::left << std::setw(10) << v.first
-		 << std::left << std::setw(10) << v.second.count() 
-		 << std::left << std::setw(18) << v.second.xmin()
-		 << std::left << std::setw(18) << v.second.xmax()
-		 << std::left << std::setw(18) << v.second(0.95)
-		 << std::endl;
-       v.second.dump(std::cout);
-     }    
-   }
-   std::cout << std::string(94, '-') << std::endl;
- }
-
- double NTCitem::CrsVel(sKeyPair indx, const T& intr, double p)
- {
-   oddBall(indx, intr);
-
-   // Get stanza in db
-   qpMap::iterator it = db.find(indx);
-
-   // Default value
-   if (it == db.end()) return Def;
-
-   // Get next stanza
-   uqMap::iterator jt = it->second.find(intr);
-
-   // Default value
-   if (jt == it->second.end()) return Def;
-
-
-   // Default value
-   if (! jt->second.full() ) return Def;
-
-   if (0) debug();
-
-   // Return value
-   return jt->second(p);
- }
-
- NTCitem::vcMap NTCitem::CrsVel(double p)
- {
-   vcMap ret;
-
-   for (auto v : db) {
-     for (auto u : v.second)
-       ret[v.first][u.first] = u.second(p);
-   }
-
-   // Return value
-   return ret;
- }
-
- void NTCitem::Add(sKeyPair indx, const T& intr, double val)
- {
-   oddBall(indx, intr);
-
-   // value below threshold
-   //
-   if (val<=Min) return;
-
-   // Check for initialization of Quantile
-   //
-   if (db.find(indx) == db.end()) {
-     db[indx][intr].histogram(Nequal);
-     for (auto v : qs) db[indx][intr].quantile(v);
-   }
-
-   // Check for intr in map
-   //
-   if (db[indx].find(intr) == db[indx].end()) {
-     db[indx][intr].histogram(Nequal);
-     for (auto v : qs) db[indx][intr].quantile(v);
-   }
-
-   if (0) {
-     std::ostringstream sout; sout << "<" << val << ">";
-
-     std::cout << "Adding " << std::setw(16) << sout.str() << " to "
-	       << indx << " " << intr
-	       << " [" << db[indx][intr].datums() 
-	       << "/"  << db[indx][intr].target() << "]";
-     if (db[indx][intr].full()) 
-       std::cout << " P(0.5) =" 
-		 << std::setw(16) << db[indx][intr](0.5)
-		 << " P(0.95) =" 
-		 << std::setw(16) << db[indx][intr](0.95)
-		 << " xmax =" 
-		 << std::setw(16) << db[indx][intr].xmax();
-     std::cout << std::endl;
-   }
-
-   // Add new element
-   //
-   db[indx][intr].add(val);
- }
+void NTCitem::Add(sKeyPair indx, const T& intr, double val)
+{
+  oddBall(indx, intr);
+  
+  // value below threshold
+  //
+  if (val<=Min) return;
+  
+  // Check for initialization of Quantile
+  //
+  if (db.find(indx) == db.end()) {
+    db[indx][intr].histogram(Nequal);
+    for (auto v : qs) db[indx][intr].quantile(v);
+  }
+  
+  // Check for intr in map
+  //
+  if (db[indx].find(intr) == db[indx].end()) {
+    db[indx][intr].histogram(Nequal);
+    for (auto v : qs) db[indx][intr].quantile(v);
+  }
+  
+  if (0) {
+    std::ostringstream sout; sout << "<" << val << ">";
+    
+    std::cout << "Adding " << std::setw(16) << sout.str() << " to "
+	      << indx << " " << intr
+	      << " [" << db[indx][intr].datums() 
+	      << "/"  << db[indx][intr].target() << "]";
+    if (db[indx][intr].full()) 
+      std::cout << " P(0.5) =" 
+		<< std::setw(16) << db[indx][intr](0.5)
+		<< " P(0.95) =" 
+		<< std::setw(16) << db[indx][intr](0.95)
+		<< " xmax =" 
+		<< std::setw(16) << db[indx][intr].xmax();
+    std::cout << std::endl;
+  }
+  
+  // Add new element
+  //
+  db[indx][intr].add(val);
+}
 
 
- NTCitem& NTCdb::operator[](const key_type& k)
- {
-   NTCdata::iterator it = data.find(k);
-
-   if (it == data.end()) {
+NTCitem& NTCdb::operator[](const key_type& k)
+{
+  NTCdata::iterator it = data.find(k);
+  
+  if (it == data.end()) {
     
     // Look for an initialization parent
     //
@@ -302,14 +302,14 @@ std::ostream& operator<< (std::ostream& out, const sKeyPair& t)
       check = check >> 3;
       it = data.find(check);
     }
-
+    
     // This is only informational, not a problem
     if (0) {
       if (DEBUG_V && it == data.end()) {
 	std::cout << "NTC: no parent found for " << k << std::endl;
       }
     }
-
+    
     // If none, create an empty item.  Otherwise, initialize from the
     // parent
     //
@@ -318,8 +318,8 @@ std::ostream& operator<< (std::ostream& out, const sKeyPair& t)
       it = data.find(k);
     }
   }
-
+  
   if (DEBUG_V) it->second.setKey(k);
-
+  
   return it->second;
 }
