ba38fedac79d7663f3e6e33f3ac547d7b2814864
Martin D. Weinberg <weinberg@astro.umass.edu>
2017-08-22 09:32:16 -0400

    Missing endl

--- src/user/CollideIon.cc
+++ src/user/CollideIon.cc
@@ -579,7 +579,7 @@ CollideIon::CollideIon(ExternalForce *force, Component *comp,
 	      << (use_spectrum ? (wvlSpect ? "in wavelength" : "in eV")
 		  : "off")                              << std::endl
 	      <<  " " << std::setw(20) << std::left << "Photoionization"
-	      << Ion::getIBtype()
+	      << Ion::getIBtype()                       << std::endl
 	      << " " << std::setw(20) << std::left << "random seed"
 	      << seed                                   << std::endl
 	      << "************************************" << std::endl;
