e7e101dc10ff8f8048b34e4062a9cb623a161476
Martin D. Weinberg <weinberg@astro.umass.edu>
2018-10-03 16:22:38 -0400

    Fix pairs computation in Trace

--- src/user/CollideIon.cc
+++ src/user/CollideIon.cc
@@ -17956,7 +17956,7 @@ Collide::sKey2Amap CollideIon::generateSelectionTrace
   double totPairs = num * dens * dfac *
     (1.0/PiProb[id][0] + 1.0/PiProb[id][1] + 1.0/PiProb[id][2]);
 
-  totPairs = 0.5*num*(num-1);
+  totPairs = 0.5*(num + 0.5);
 
   colCf[id] = selcM/totPairs;
   selcM = totPairs;
