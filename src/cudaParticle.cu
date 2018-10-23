3e2df337e6ab9de5b0310a25e869c98188c27381
Martin D. Weinberg <weinberg@astro.umass.edu>
2018-10-22 20:31:23 -0400

    Updates to dattrib array handling

--- src/cudaParticle.cu
+++ src/cudaParticle.cu
@@ -13,15 +13,24 @@ void ParticleHtoD(PartPtr h, cudaParticle & d, int beg, int end)
   }
   d.pot    = h->pot;
   d.potext = h->potext;
-#ifdef DATTRIB_CUDA
-  if (end) {			// Skip attributes if end is 0
-    if (end<h->dattrib.size()) {
-      for (int n=beg; n<end; n++) d.datr[n-beg] = h->dattrib[n];
+#if DATTRIB_CUDA>0
+  // Skip attributes if end is 0
+  //
+  if (end) {
+    if (end - beg < DATTRIB_CUDA) {
+      if (end<h->dattrib.size()) {
+	for (int n=beg; n<end; n++) d.datr[n-beg] = h->dattrib[n];
+      } else {
+	std::cerr << "Wrong attribute size in ParticleHtoD" << std::endl;
+      }
     } else {
-      std::cerr << "Wrong attribute size in ParticleHtoD" << std::endl;
+	std::cerr << "Requested number of attributes ["
+		  << end - beg << "] in ParticleHtoD is larger than storage="
+		  << DATTRIB_CUDA << std::endl;
     }
   }
 #endif
+  d.dtreq  = h->dtreq;
   d.level  = h->level;
   d.indx   = h->indx;
 }
@@ -36,11 +45,22 @@ void ParticleDtoH(const cudaParticle & d, PartPtr h, int beg, int end)
   }
   h->pot    = d.pot;
   h->potext = d.potext;
-#ifdef DATTRIB_CUDA
+#if DATTRIB_CUDA>0
   if (end) {
-    for (int n=beg; n<end; n++) h->dattrib[n] = d.datr[n-beg];
+    if (end - beg < DATTRIB_CUDA) {
+      if (end < h->dattrib.size()) {
+	for (int n=beg; n<end; n++) h->dattrib[n] = d.datr[n-beg];
+      } else {
+	std::cerr << "Wrong attribute size in ParticleDtoH" << std::endl;
+      }
+    } else {
+	std::cerr << "Requested number of attributes ["
+		  << end - beg << "] in ParticleDtoH is larger than storage="
+		  << DATTRIB_CUDA << std::endl;
+    }
   }
 #endif
+  h->dtreq  = d.dtreq;
   h->level  = d.level;
   h->indx   = d.indx;
 }
@@ -57,6 +77,10 @@ std::ostream& operator<< (std::ostream& os, const cudaParticle& p)
   // for (int k=0; k<3; k++) os << std::setw(16) << p.vel[k];
   for (int k=0; k<3; k++) os << std::setw(16) << p.acc[k];
   // os << std::setw(16) << p.pot << std::setw(16) << p.potext;
+#if DATTRIB_CUDA>0
+  for (int n=0; n<DATTRIB_CUDA; n++) os << std::setw(16) << p.datr[n];
+#endif
+
   os.precision(sp);
   return os;
 }
