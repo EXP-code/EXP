bf8fd7635585e520e6ad52d3f72c7d4d04d451ab
Martin D. Weinberg <weinberg@astro.umass.edu>
2019-04-06 11:34:55 -0400

    Fixed unintialized coefficient accumulation for quadruature

--- utils/ICs/initial2.cc
+++ utils/ICs/initial2.cc
@@ -1,5 +1,3 @@
-// #define DEBUG
-
 /*
   Generates a Monte Carlo realization of a halo with an embedded
   disk using Jeans' equations.
@@ -78,6 +76,8 @@
 #include <sstream>
 #include <string>
 #include <vector>
+
+#include <fenv.h>
 
 // Boost stuff
 //
@@ -245,6 +245,7 @@ main(int ac, char **av)
   int          MMAX;
   int          NUMX;
   int          NUMY;
+  int          NOUT;
   int          NORDER;
   int          NORDER1;
   bool         SELECT;
@@ -329,6 +330,7 @@ main(int ac, char **av)
     ("NUMY",            po::value<int>(&NUMY)->default_value(128),                      "Vertical grid size for disk basis table")
     ("NORDER",          po::value<int>(&NORDER)->default_value(16),                     "Number of disk basis functions per M-order")
     ("NORDER1",         po::value<int>(&NORDER1)->default_value(1000),                  "Restricts disk basis function to NORDER1<NORDER after basis construction for testing")
+    ("NOUT",            po::value<int>(&NOUT)->default_value(1000),                      "Number of radial basis functions to output for each harmonic order")
     ("SELECT",          po::value<bool>(&SELECT)->default_value(false),                 "Enable significance selection in coefficient computation")
     ("DUMPCOEF",        po::value<bool>(&DUMPCOEF)->default_value(false),               "Dump disk coefficients")
     ("DIVERGE",         po::value<int>(&DIVERGE)->default_value(0),                     "Cusp extrapolation for primary halo model")
@@ -373,6 +375,7 @@ main(int ac, char **av)
     ("halofile1",       po::value<string>(&halofile1)->default_value("SLGridSph.model"),        "File with input halo model")
     ("halofile2",       po::value<string>(&halofile2)->default_value("SLGridSph.model.fake"),   "File with input halo model for multimass")
     ("cachefile",       po::value<string>(&cachefile)->default_value(".eof.cache.file"),        "Name of EOF cache file")
+    ("runtag",          po::value<string>(&runtag)->default_value("run000"),                    "Label for diagnostic images")
     ("report",          po::value<bool>(&report)->default_value(true),                  "Report particle progress in EOF computation")
     ("ignore",          po::value<bool>(&ignore)->default_value(false),                 "Ignore any existing cache file and recompute the EOF")
     ;
@@ -422,6 +425,8 @@ main(int ac, char **av)
       MPI_Finalize();
       return 2;
     }
+
+    NOUT = std::min<int>(NOUT, NORDER);
 
     // Write template file
     //
@@ -572,8 +577,7 @@ main(int ac, char **av)
   if (n_particlesH) {
     expandh = new SphericalSL(nthrds, LMAX, NMAX, SCSPH);
 #ifdef DEBUG
-    string dumpname("debug");
-    expandh->dump_basis(dumpname);
+    expandh->dump_basis(runtag);
 #endif
   }
 
@@ -585,6 +589,7 @@ main(int ac, char **av)
   EmpCylSL::NUMX        = NUMX;
   EmpCylSL::NUMY        = NUMY;
   EmpCylSL::NUMR        = NUMR;
+  EmpCylSL::NOUT        = NOUT;
   EmpCylSL::CMAP        = CMAP;
   EmpCylSL::VFLAG       = VFLAG;
   EmpCylSL::logarithmic = LOGR;
@@ -762,9 +767,9 @@ main(int ac, char **av)
   
   if (n_particlesD) {
     if (myid==0) cout << "Beginning disk accumulation . . . " << flush;
+    expandd->setup_accumulation();
     if (!save_eof and !expcond) {
       expandd->setup_eof();
-      expandd->setup_accumulation();
       if (nthrds>1)
 	expandd->accumulate_eof_thread(dparticles, report);
       else
@@ -834,12 +839,10 @@ main(int ac, char **av)
     
     if (n_particlesD) {
       int nout = 200;
-      char dumpname[] = "basis.dump";
-      expandd->dump_basis(dumpname, 0);
-      string prefix = "gendisk2";
-      expandd->dump_images(prefix, 5.0*scale_length, 5.0*scale_height,
+      expandd->dump_basis(runtag, 0);
+      expandd->dump_images(runtag, 5.0*scale_length, 5.0*scale_height,
 			   nout, nout, false);
-      expandd->dump_images_basis(prefix, 5.0*scale_length, 5.0*scale_height,
+      expandd->dump_images_basis(runtag, 5.0*scale_length, 5.0*scale_height,
 				 nout, nout, false, 0, MMAX, 0, NORDER-1);
     }
 
