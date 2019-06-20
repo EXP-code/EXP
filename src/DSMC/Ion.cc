74d0b0847647e15e78676f36559c0bee13476b62
Martin D. Weinberg <weinberg@astro.umass.edu>
2019-06-19 21:34:51 -0400

    Increase the maximum grid energy for cuda textures

--- src/DSMC/Ion.cc
+++ src/DSMC/Ion.cc
@@ -82,7 +82,7 @@ bool   Ion::useIonizeGrid    = true;
 bool   Ion::GridDebug        = false; // Set to true for debugging
 int    Ion::GridReport       = 10000; // Used for debugging only
 double Ion::EminGrid         = 0.001; // eV
-double Ion::EmaxGrid         = 50.0;  // eV
+double Ion::EmaxGrid         = 500.0; // eV
 double Ion::DeltaEGrid       = 0.05;  // eV
 
 // Chianti element list
