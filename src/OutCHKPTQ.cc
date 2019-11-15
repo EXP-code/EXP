986708fc58d034a3e8c0aae0b3f4c9aa42034c70
Martin D. Weinberg <weinberg@astro.umass.edu>
2019-11-14 19:44:44 -0500

    Split checkpoint file implementation

---
+++ src/OutCHKPTQ.cc
@@ -0,0 +1,277 @@
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+#include <chrono>
+
+#include "expand.h"
+#include <global.H>
+
+#include <AxisymmetricBasis.H>
+#include <OutCHKPTQ.H>
+
+
+OutCHKPTQ::OutCHKPTQ(const YAML::Node& conf) : Output(conf)
+{
+  initialize();
+}
+
+void OutCHKPTQ::initialize()
+{
+  try {
+
+    if (Output::conf["mpio"])
+      mpio = Output::conf["mpio"].as<bool>();
+    else
+      mpio = false;
+				// Get file name
+    if (Output::conf["filename"])
+      filename = Output::conf["filename"].as<std::string>();
+    else {
+      filename.erase();
+      filename = outdir + "SPL." + runtag + ".chkpt";
+    }
+    
+    if (Output::conf["nint"])
+      nint = Output::conf["nint"].as<int>();
+    else
+      nint = 100;
+    
+    if (Output::conf["timer"])
+      timer = Output::conf["timer"].as<bool>();
+    else
+      timer = false;
+
+    if (Output::conf["nagg"])
+      nagg = Output::conf["nagg"].as<std::string>();
+    else
+      nagg = "1";
+  }
+  catch (YAML::Exception & error) {
+    if (myid==0) std::cout << "Error parsing parameters in OutCHKPTQ: "
+			   << error.what() << std::endl
+			   << std::string(60, '-') << std::endl
+			   << "Config node"        << std::endl
+			   << std::string(60, '-') << std::endl
+			   << Output::conf         << std::endl
+			   << std::string(60, '-') << std::endl;
+    MPI_Finalize();
+    exit(-1);
+  }
+}
+
+
+void OutCHKPTQ::Run(int n, bool last)
+{
+  if (n % nint && !last) return;
+  if (VERBOSE>5 && myid==0) {
+    cout << " OutCHKPTQ::Run(): n=" << n << " psdump=" << psdump << endl;
+  }
+
+  int returnStatus = 1;
+  
+  if (n == psdump) {       
+
+    if (myid==0) {
+      string backfile = filename + ".bak";
+      if (unlink(backfile.c_str())) {
+	if (VERBOSE>5) perror("OutCHKPTQ::Run()");
+	cout << "OutCHKPTQ::Run(): error unlinking old backup file <" 
+	     << backfile << ">, it may not exist" << endl;
+      } else {
+	if (VERBOSE>5) {
+	  cout << "OutCHKPTQ::Run(): successfully unlinked <"
+	       << backfile << ">" << endl;
+	}
+      }
+      if (rename(filename.c_str(), backfile.c_str())) {
+	if (VERBOSE>5) perror("OutCHKPTQ::Run()");
+	cout << "OutCHKPTQ: renaming backup file <" 
+	     << backfile << ">, it may not exist" << endl;
+      } else {
+	if (VERBOSE>5) {
+	  cout << "OutCHKPTQ::Run(): successfully renamed <"
+	       << filename << "> to <" << backfile << ">" << endl;
+	}
+      }
+      if (symlink(lastPSQ.c_str(), filename.c_str())) {
+	if (VERBOSE>5) perror("OutCHKPTQ::Run()");
+	cout << "OutCHKPTQ::Run(): no file <" << lastPSQ
+	     << "> to link, we will create a new checkpoint" << endl;
+	returnStatus = 0;
+      } else {
+	if (VERBOSE>5) {
+	  cout << "OutCHKPTQ::Run(): successfully linked <"
+	       << lastPSQ << "> to new backup file <" 
+	       << filename << ">" << endl;
+	}
+      }
+
+      int count = 0;
+      for (auto c : comp->components) {
+				// Component file
+	std::ostringstream cname;
+	cname << filename << "_" << count;
+    
+	for (int n=0; n<numprocs; n++) {
+
+	  std::ostringstream sout1;
+	  sout1 << cname.str() << "-" << n;
+	  std::string fileN = sout1.str();
+
+	  sout1 << ".bak";
+	  std::string backN = sout1.str();
+
+	  if (unlink(backN.c_str())) {
+	    if (VERBOSE>5) perror("OutCHKPTQ::Run()");
+	    cout << "OutCHKPTQ::Run(): error unlinking old backup file <" 
+		 << backN << ">, it may not exist" << endl;
+	  } else {
+	    if (VERBOSE>5) {
+	      cout << "OutCHKPTQ::Run(): successfully unlinked <"
+		   << backN << ">" << endl;
+	    }
+	  }
+	  if (rename(fileN.c_str(), backN.c_str())) {
+	    if (VERBOSE>5) perror("OutCHKPTQ::Run()");
+	    cout << "OutCHKPTQ: renaming backup file <" 
+		 << backN << ">, it may not exist" << endl;
+	  } else {
+	    if (VERBOSE>5) {
+	    cout << "OutCHKPTQ::Run(): successfully renamed <"
+		 << filename << "> to <" << backN << ">" << endl;
+	    }
+	  }
+
+	  std::ostringstream sout2;
+	  sout2 << lastPSQ << "_" << count << "-" << n;
+	  std::string lastN = sout2.str();
+	  
+	  if (symlink(lastN.c_str(), fileN.c_str())) {
+	    if (VERBOSE>5) perror("OutCHKPTQ::Run()");
+	    cout << "OutCHKPTQ::Run(): no file <" << lastN
+		 << "> to link, we will create a new checkpoint" << endl;
+	    returnStatus = 0;
+	  } else {
+	    if (VERBOSE>5) {
+	      cout << "OutCHKPTQ::Run(): successfully linked <"
+		   << lastN << "> to new backup file <" 
+		   << fileN << ">" << endl;
+	    }
+	  }
+	} // END: process loop
+
+	count++;
+      } // END: component loop
+
+    } // myid==0
+
+  } // n == psdump
+
+  MPI_Bcast(&returnStatus, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  if (returnStatus==1) return;
+
+  std::chrono::high_resolution_clock::time_point beg, end;
+  if (timer) beg = std::chrono::high_resolution_clock::now();
+  
+  psdump = n;
+
+  int nOK = 0;
+
+  std::ofstream out;
+
+  if (myid==0) {
+				// Open file and write master header
+    out.open(filename);
+
+    if (out.fail()) {
+      std::cerr << "OutCHKPTQ: can't open file <" << filename
+		<< "> . . . quitting" << std::endl;
+      nOK = 1;
+    }
+				// Open file and write master header
+    if (nOK==0) {
+      struct MasterHeader header;
+      header.time  = tnow;
+      header.ntot  = comp->ntot;
+      header.ncomp = comp->ncomp;
+
+      out.write((char *)&header, sizeof(MasterHeader));
+    }
+  }
+  
+  MPI_Bcast(&nOK, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  if (nOK) {
+    MPI_Finalize();
+    exit(33);
+  }
+
+  int count = 0;
+  for (auto c : comp->components) {
+				// Check for open failures
+    nOK = 0;
+
+				// Component file
+    std::ostringstream cname;
+    cname << filename << "_" << count++;
+    
+    if (myid==0) {
+      c->write_binary_header(&out, false, cname.str());
+    }
+
+    cname << "-" << myid;
+    
+				// Open file and write master header
+    std::ofstream pout(cname.str());
+
+    if (pout.fail()) {
+      std::cerr << "[" << myid << "] OutCHKPTQ: can't open file <" << cname.str() 
+		<< "> . . . quitting" << std::endl;
+      nOK = 1;
+    } else {
+      c->write_binary_particles(&pout, false);
+      if (pout.fail()) {
+	std::cout << "OutCHKPTQ: error writing binary particles to <"
+		  << cname.str() << std::endl;
+      }
+    }
+
+    
+				// Check for errors in all file opening
+    int sumOK;
+    MPI_Allreduce(&nOK, &sumOK, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+    if (sumOK) {
+      MPI_Finalize();
+      exit(35);
+    }
+  }
+
+  if (myid==0) {
+    if (out.fail()) {
+      std::cout << "OutCHKPTQ: error writing component to master <" << filename
+		<< std::endl;
+    }
+
+    try {
+      out.close();
+    }
+    catch (const ofstream::failure& e) {
+      std::cout << "OutCHKPTQ: exception closing file <" << filename
+		<< ": " << e.what() << std::endl;
+    }
+  }
+
+  chktimer.mark();
+
+
+  if (timer) {
+    end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> intvl = end - beg;
+      if (myid==0)
+	std::cout << "OutCHKPTQ [T=" << tnow << "] timing=" << intvl.count()
+		  << std::endl;
+  }
+}
+
