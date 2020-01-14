7e49b4502a57205076e49fc36faea0d26c668dca
Martin D. Weinberg <weinberg@astro.umass.edu>
2020-01-14 14:39:23 -0500

    Added yaml_diff tool for comparing config files

---
+++ utils/Analysis/yaml_diff.cc
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+
+#include <yaml-cpp/yaml.h>
+
+void usage(char * prog)
+{
+  std::cout << "USAGE:" << std::endl
+	    << prog << " <fiducial file> <comparison file>" << std::endl
+	    << std::endl
+	    << "This routine recursively checks every node in the 'fiducial file' against the " << std::endl
+	    << "nodes in the 'comparison file' and missing nodes and different values." << std::endl
+	    << "Different values in the comparison file will be printed as \e[1;31m != value\e[0m," << std::endl
+	    << "while missing nodes in the comparison file will be denoted by the suffix \e[1;34m[***]\e[0m" << std::endl << std::endl
+	    << "It may be useful to reverse the order of the files for a full comparsion." << std::endl << std::endl
+	    << "Example: " << prog << " file1.yaml file2.yaml | less -R" << std::endl << std::endl;
+  exit(-1);
+}
+
+
+const int indent = 2;
+
+void recurse(YAML::Node& config1, YAML::Node& config2, int level)
+{
+  int cnt = 0;
+
+  for (YAML::iterator it=config1.begin(); it!=config1.end(); it++) {
+
+    YAML::Node cur1, cur2;
+
+    if (config1.IsSequence()) {	// This is a YAML sequence
+      std::cout << std::setw(level*indent) << "" << "- ";
+      cur1 = config1[cnt];
+      cur2 = config2[cnt];
+      cnt++;
+    } else {			// Otherwise, a YAML map
+      std::cout << std::setw(level*indent) << "" << "+ " << it->first;
+      std::ostringstream key; key << it->first;
+      cur1 = it->second;
+      cur2 = config2[key.str()];
+    }
+
+				// Check node type, if the fiducial
+				// node exists
+    if (cur1) {
+      switch (cur1.Type()) {
+      case YAML::NodeType::Null:
+	break;
+      case YAML::NodeType::Scalar:
+	std::cout << ": " << cur1;
+	if (cur2) {		// If the comparison node exists,
+				// compare the values
+	  std::ostringstream sout1; sout1 << cur1;
+	  std::ostringstream sout2; sout2 << cur2;
+	  if (sout2.str().find(sout1.str())) {
+	    std::cout << "\e[1;31m != " << sout2.str() << "\e[0m";
+	  }
+	} else {		// Flag missing node in comparison
+				// file
+	  std::cout << "\e[1;36m [***]\e[0m";
+	}
+	std::cout << std::endl;
+	break;
+      case YAML::NodeType::Sequence:
+      case YAML::NodeType::Map:
+	if (cur2) {		// Recurse if the node is a container
+	  std::cout << std::endl;
+	  recurse(cur1, cur2, level+1);
+	} else {
+	  std::cout << "\e[1;34m [***]\e[0m" << std::endl;
+	}
+	break;
+      case YAML::NodeType::Undefined:
+	std::cout << ": Undefined" << std::endl;
+      }
+    } else {
+      std::cout << std::endl;
+    }
+  }
+
+  return;
+}
+
+int main(int argc, char** argv)
+{
+  if (argc != 3) usage(argv[0]);
+
+  std::ifstream InFile1, InFile2;
+  InFile1.exceptions(std::ios::failbit);
+  InFile2.exceptions(std::ios::failbit);
+
+  try {
+    InFile1.open(argv[1]);
+    InFile2.open(argv[2]);
+
+    YAML::Node      config1 = YAML::Load(InFile1);
+    YAML::Node      config2 = YAML::Load(InFile2);
+    std::string     key, val;
+
+    recurse(config1, config2, 0);
+  }
+  catch (std::ios_base::failure& fail) {
+    std::cout << "Failure to open <" << argv[1] << "> or <" << argv[2] << ">" << std::endl
+	      << "Error: " << fail.what() << std::endl;
+  }
+  catch (YAML::Exception& error) {
+    std::cout << "YAML failure" << std::endl
+	      << "Error: " << error.what() << std::endl;
+  }
+
+  return 0;
+};
