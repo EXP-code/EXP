d313aac5e682ac82b4d7efbe82732908dbb1c51f
Martin D. Weinberg <weinberg@astro.umass.edu>
2020-05-23 13:26:56 -0400

    Convert to MPI

--- utils/PhaseSpace/psp2bess.cc
+++ utils/PhaseSpace/psp2bess.cc
@@ -25,7 +25,17 @@
 #include <boost/progress.hpp>
 #include <boost/math/special_functions/bessel.hpp>
 
+#include <mpi.h>
+
 namespace po = boost::program_options;
+
+//
+// MPI variables
+//
+int numprocs, myid, proc_namelen;
+char processor_name[MPI_MAX_PROCESSOR_NAME];
+
+
 
 //! Generate orthonormal Bessel functions of integral order
 class Bess
@@ -118,6 +128,22 @@ public:
   //! Add a particle to coefficient
   void add(double mass, double R, double phi, double vr, double vt, double vz);
 
+  //! MPI synchronize
+  void synchronize()
+  {
+    MPI_Allreduce(MPI_IN_PLACE, &maccum, 1, MPI_DOUBLE, MPI_SUM,
+		  MPI_COMM_WORLD);
+    for (int m=mmin; m<=mmax; m++) {
+      for (size_t k=0; k<3; k++) {
+	MPI_Allreduce(MPI_IN_PLACE, cos_c[m][k].data(), nmax, MPI_DOUBLE,
+		      MPI_SUM, MPI_COMM_WORLD);
+	if (m)
+	  MPI_Allreduce(MPI_IN_PLACE, sin_c[m][k].data(), nmax, MPI_DOUBLE,
+			MPI_SUM, MPI_COMM_WORLD);
+      } // END: k=0,...,2
+    } // END: m loop
+  }
+
   //! Normalize by total mass
   void normalize()
   {
@@ -132,6 +158,7 @@ public:
       }
     }
   }
+
 
   //! Write binary file
   void write(std::ostream& out);
@@ -192,7 +219,6 @@ BessCoefs::add(double mass, double R, double phi, double vr, double vt, double v
 
 				// Globals for exputil library
 				// Unused here
-int myid = 0;
 char threading_on = 0;
 pthread_mutex_t mem_lock;
 string outdir, runtag;
@@ -200,6 +226,15 @@ string outdir, runtag;
 int
 main(int ac, char **av)
 {
+  //===================
+  // MPI preliminaries
+  //===================
+
+  MPI_Init(&ac, &av);
+  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+  MPI_Get_processor_name(processor_name, &proc_namelen);
+
   char *prog = av[0];
   bool verbose = false;
   std::string cname, tname, new_dir, suffix, work_dir;
@@ -244,15 +279,17 @@ main(int ac, char **av)
     po::store(po::parse_command_line(ac, av, desc), vm);
     po::notify(vm);    
   } catch (po::error& e) {
-    std::cout << "Option error: " << e.what() << std::endl;
+    if (myid==0) std::cout << "Option error: " << e.what() << std::endl;
     exit(-1);
   }
 
   if (vm.count("help")) {
-    std::cout << desc << std::endl;
-    std::cout << "Example: " << std::endl;
-    std::cout << "\t" << av[0]
-	      << " --output=out.bod" << std::endl;
+    if (myid==0) {
+      std::cout << desc << std::endl;
+      std::cout << "Example: " << std::endl;
+      std::cout << "\t" << av[0]
+		<< " --runtag=run001" << std::endl;
+    }
     return 1;
   }
 
@@ -270,20 +307,26 @@ main(int ac, char **av)
     std::string file = fname.str();
 
     if (!FileExists(file)) {
-      std::cerr << "Error opening file <" << file << "> for input"
-		<< std::endl;
+      if (myid==0) {
+	std::cerr << "Error opening file <" << file << "> for input"
+		  << std::endl;
+      }
       break;
     }
   }
   iend = n-1;
-  std::cerr << "Assuming last file has index <" << iend << ">"
-	    << std::endl;
+  if (myid==0) {
+    std::cerr << "Assuming last file has index <" << iend << ">"
+	      << std::endl;
+  }
 
   std::string outcoefs = work_dir + "/" + runtag + "." + suffix;
   std::ofstream out(outcoefs);
   if (!out) {
-    std::cerr << "Error opening file <" << outcoefs << "> for output"
-	      << std::endl;
+    if (myid==0) {
+      std::cerr << "Error opening file <" << outcoefs << "> for output"
+		<< std::endl;
+    }
     exit(-1);
   }
 
@@ -302,8 +345,10 @@ main(int ac, char **av)
       break;
     }
 
-    if (verbose) cerr << "Using filename: " << file << endl;
-    else ++progress;
+    if (myid==0) {
+      if (verbose) cerr << "Using filename: " << file << endl;
+      else ++progress;
+    }
 
 				// Parse the PSP file
 				// ------------------
@@ -316,7 +361,7 @@ main(int ac, char **av)
 
 				// Now write a summary
 				// -------------------
-    if (verbose) {
+    if (verbose and myid==0) {
       
       psp->PrintSummary(cerr);
     
@@ -342,8 +387,11 @@ main(int ac, char **av)
     
       if (stanza->name != cname) continue;
 
+      unsigned int icnt = 0;
       for (part=psp->GetParticle(); part!=0; part=psp->NextParticle()) {
 	
+	if (icnt % numprocs) continue;
+
 	// Cylindrical radius
 	//
 	double val = 0.0;
@@ -372,12 +420,15 @@ main(int ac, char **av)
       }
     }
 
-    // Normalize
+    // Prepare for output
     //
-    bess.normalize();
-    bess.write(out);
+    bess.synchronize();
+    if (myid==0) {
+      bess.normalize();
+      bess.write(out);
+    }
   }
-  std::cout << std::endl;
+  if (myid==0) std::cout << std::endl;
 
   return 0;
 }
