d91b772c24494831e7931845cdff7326722aa2b0
Martin D. Weinberg <weinberg@astro.umass.edu>
2021-01-30 22:02:38 -0500

    Whitespace and comments only

--- src/OutPSP.cc
+++ src/OutPSP.cc
@@ -101,7 +101,7 @@ void OutPSP::Run(int n, int mstep, bool last)
   if (!dump_signal and !last) {
     if (n % nint            ) return;
     if (restart  && n==0    ) return;
-    if (mstep % nintsub !=0 )  return;
+    if (mstep % nintsub !=0 ) return;
   }
 
   std::chrono::high_resolution_clock::time_point beg, end;
