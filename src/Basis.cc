43828bad73dbd80e2657a23265a11037a5b08f48
Martin D. Weinberg <weinberg@astro.umass.edu>
2021-05-17 12:19:10 -0400

    Allow mmax=0 evaluation

--- src/Basis.cc
+++ src/Basis.cc
@@ -100,12 +100,14 @@ void Basis::sinecosine_R(int mmax, double phi, Vector& c, Vector& s)
   c[0] = 1.0;
   s[0] = 0.0;
 
-  c[1] = cos(phi);
-  s[1] = sin(phi);
+  if (mmax>0) {
+    c[1] = cos(phi);
+    s[1] = sin(phi);
 
-  for (m=2; m<=mmax; m++) {
-    c[m] = 2.0*c[1]*c[m-1] - c[m-2];
-    s[m] = 2.0*c[1]*s[m-1] - s[m-2];
+    for (m=2; m<=mmax; m++) {
+      c[m] = 2.0*c[1]*c[m-1] - c[m-2];
+      s[m] = 2.0*c[1]*s[m-1] - s[m-2];
+    }
   }
 }
 
