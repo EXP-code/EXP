30296d5a5a2c1364969f18af34f6223a69f195aa
Martin D. Weinberg <weinberg@astro.umass.edu>
2021-09-16 08:48:04 -0400

    Fix linking problem on unity

--- autogen.sh
+++ autogen.sh
@@ -15,31 +15,15 @@ else
 echo Found GNU Make at $MAKE ... good.
 fi
 
-echo This script runs configure and make...
-echo You did remember necessary arguments for configure, right?
+echo This script runs git, cmake and make...
+echo You did remember necessary arguments for cmake, right?
 
-if test ! -x `which aclocal`
-then echo you need autoconfig and automake to generate the Makefile
+if test ! -x `which cmake`
+then echo you need cmake to generate Makefiles
 fi
 
-if test ! -x `which automake`
-then echo you need automake to generate the Makefile
-fi
-
-if test ! -x `which autoconf`
-then echo you need autoconf to generate the Makefile
-fi
-
-if test ! -x `which libtoolize`
-then echo you need libtoolize to generate the Makefile
-fi
-
-aclocal -I m4
-autoheader
-libtoolize --ltdl --copy --force
-automake --add-missing --copy
-automake
-autoconf
-./configure $*
-$MAKE CXXFLAGS="-O3" CFLAGS="-O3" -j$NPROC
+mkdir -p build
+cd build
+CC=gcc CXX=g++ cmake -DCUDA_USE_STATIC_CUDA_RUNTIME=off -DCMAKE_CUDA_ARCHITECTURES="72" -DENABLE_CUDA=1 -DEigen3_DIR=$EIGEN_BASE/share/eigen3/cmake -Wno-dev ..
+make -j $((2*$(NPROC)))
 
