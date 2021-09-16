54a3767910596846060ace391e4aec6dc39bf11c
Martin D. Weinberg <weinberg@astro.umass.edu>
2021-09-16 10:40:11 -0400

    Updated autogen.sh for CMake

--- autogen.sh
+++ autogen.sh
@@ -16,14 +16,27 @@ echo Found GNU Make at $MAKE ... good.
 fi
 
 echo This script runs git, cmake and make...
-echo You did remember necessary arguments for cmake, right?
+echo This example assumes a GPU/CUDA build.  Adjust the arguments
+echo according to your needs.  The documentation is no longer make
+echo by default.  Execute "make doc" to generate the doxygen html.
 
 if test ! -x `which cmake`
 then echo you need cmake to generate Makefiles
 fi
 
 mkdir -p build
+
 cd build
-CC=gcc CXX=g++ cmake -DCUDA_USE_STATIC_CUDA_RUNTIME=off -DCMAKE_CUDA_ARCHITECTURES="72" -DENABLE_CUDA=1 -DEigen3_DIR=$EIGEN_BASE/share/eigen3/cmake -Wno-dev ..
+
+# With cuda
+cmake -DCUDA_USE_STATIC_CUDA_RUNTIME=off -DCMAKE_CUDA_ARCHITECTURES="72" -DENABLE_CUDA=1 -DEigen3_DIR=$EIGEN_BASE/share/eigen3/cmake -Wno-dev ..
 make -j $((2*$(NPROC)))
 
+# No cuda
+cmake -DEigen3_DIR=$EIGEN_BASE/share/eigen3/cmake -Wno-dev ..
+
+# Make the package
+make -j $((2*$(NPROC)))
+
+# Make the documentation
+# make doc
