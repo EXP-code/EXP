3ca5eb86dcc387f2874a79210b98b7bba9fc88a0
Martin D. Weinberg <weinberg@astro.umass.edu>
2021-12-16 16:15:21 -0500

    When used with mpirun, only root should print help messages

--- utils/Analysis/haloprof.cc
+++ utils/Analysis/haloprof.cc
@@ -623,12 +623,16 @@ main(int argc, char **argv)
   // ==================================================
 
   if (vm.count("help")) {
-    std::cout << std::endl << options.help({""}) << std::endl;
+    if (myid==0)
+      std::cout << std::endl << options.help({""}) << std::endl;
+    MPI_Finalize();
     return 0;
   }
 
   if (!vm.count("help") and vm.count("expert")) {
-    std::cout << std::endl << options.help({"", "expert"}) << std::endl;
+    if (myid==0)
+      std::cout << std::endl << options.help({"", "expert"}) << std::endl;
+    MPI_Finalize();
     return 0;
   }
 
