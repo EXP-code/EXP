37b1f9942e7773c363914bc7ffc1271c6cde82f5
Martin D. Weinberg <weinberg@astro.umass.edu>
2022-05-11 23:10:28 -0400

    A few more comments added to libvars.cc

--- exputil/libvars.cc
+++ exputil/libvars.cc
@@ -7,19 +7,34 @@
 
 namespace __EXP__
 {
+  //@{
+  //! POSIX thread support
   char             threading_on     = 0;
   pthread_mutex_t  mem_lock;
+  //@}
+
+  //! Location for output
   std::string      outdir           = "./";
+
+  //! Run name for file labeling
   std::string      runtag           = "newrun";
-  int              nthrds           = 1; // Number of POSIX threads (minimum: 1)
 
+  //! Number of POSIX threads (minimum: 1)
+  int              nthrds           = 1;
 
+  //@{
+  //! Multistep indices
   int              this_step        = 0;
   unsigned         multistep        = 0;
   unsigned         maxlev           = 100;
   int              mstep            = 1;
   int              Mstep            = 1;
+  //@}
+
+  //! MPI node id
   int              myid             = 0;
+
+  //! Random number generator instance
   std::mt19937     random_gen;
 };
 
