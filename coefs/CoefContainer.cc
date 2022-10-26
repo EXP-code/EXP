50bf332a34e04785524b9819b359de8de121a013
Martin D. Weinberg <weinberg@astro.umass.edu>
2022-10-26 16:07:44 -0400

    Fixed Table key assignment; added additional sanity checks for key values; removed some commented out code

--- coefs/CoefContainer.cc
+++ coefs/CoefContainer.cc
@@ -94,20 +94,32 @@ namespace MSSA
     //
     keys.clear();
     for (auto v : keys0) {
-      // Sanity check
-      if (v[0]>=0 and v[0]<=mmax and
+      // Sanity check rank
+      //
+      if (v.size() != 2) {
+	std::ostringstream sout;
+	sout << "CoefDB::pack_cylinder: key vector should have rank 2; "
+	     << "found rank " << v.size() << " instead";
+	throw std::runtime_error(sout.str());
+      }
+      // Sanity check values
+      //
+      else if (v[0]>=0 and v[0]<=mmax and
 	  v[1]>=0 and v[1]<=nmax ) {
 	auto c = v, s = v;
 	c.push_back(0);
 	s.push_back(1);
 	keys.push_back(c);
 	if (v[0]) keys.push_back(s);
+      } else {
+	throw std::runtime_error("CoefDB::pack_cylinder: key is out of bounds");
       }
     }
 
     bkeys.clear();
     for (auto v : bkeys0) {
-      // Sanity check
+      // Sanity check values
+      //
       if (v[0]>=0 and v[0]<=mmax and
 	  v[1]>=0 and v[1]<=nmax ) {
 	auto c = v, s = v;
@@ -182,9 +194,19 @@ namespace MSSA
     //
     keys.clear();
     for (auto k : keys0) {
-      if (k[0] <= lmax and k[0] >= 0 and // Sanity check
-	  k[1] <= k[0] and k[1] >= 0 and
-	  k[2] <= nmax and k[2] >= 0 ) {
+      // Sanity check rank
+      //
+      if (k.size() != 3) {
+	std::ostringstream sout;
+	sout << "CoefDB::pack_sphere: key vector should have rank 3; "
+	     << "found rank " << k.size() << " instead";
+	throw std::runtime_error(sout.str());
+      }
+      // Sanity check values
+      //
+      else if (k[0] <= lmax and k[0] >= 0 and
+	       k[1] <= k[0] and k[1] >= 0 and
+	       k[2] <= nmax and k[2] >= 0 ) {
 	
 	auto v = k;
 	v.push_back(0);
@@ -197,11 +219,16 @@ namespace MSSA
 	  data[v].resize(ntimes);
 	}
       }
+      else {
+	throw std::runtime_error("CoefDB::pack_sphere: key is out of bounds");
+      }
     }
 
     bkeys.clear();
     for (auto k : bkeys0) {
-      if (k[0] <= lmax and k[0] >= 0 and // Sanity check
+      // Sanity check values
+      //
+      if (k[0] <= lmax and k[0] >= 0 and
 	  k[1] <= k[0] and k[1] >= 0 and
 	  k[2] <= nmax and k[2] >= 0 ) {
 	
@@ -271,6 +298,29 @@ namespace MSSA
     int cols    = cf->cols;
     int ntimes  = times.size();
     
+    // Promote desired keys which are the data columns
+    //
+    keys.clear();
+    for (auto v : keys0) {
+      if (v.size() != 1) {
+	std::ostringstream sout;
+	sout << "CoefDB::pack_table: key vector should have rank 1; "
+	     << "found rank " << v.size() << " instead";
+	throw std::runtime_error(sout.str());
+      }
+      else if (v[0] >= cols) {
+	std::ostringstream sout;
+	sout << "CoefDB::pack_table: requested key=" << v[0]
+	     << " exceeded the number of columns, " << cols;
+	throw std::runtime_error(sout.str());
+      }
+      else keys.push_back(v);	// OKAY
+    }
+
+    // No bkeys for a table
+    //
+    bkeys.clear();
+
     for (unsigned c=0; c<cols; c++) {
       Key key = {c};
       data[key].resize(ntimes);
@@ -281,7 +331,6 @@ namespace MSSA
 	Key key = {c};
 
 	cf = dynamic_cast<Coefs::TblStruct*>( cur->getCoefStruct(times[t]).get() );
-	keys.push_back(key);
 	data[key][t] = cf->coefs(0, c).real();
       }
     }
@@ -337,96 +386,6 @@ namespace MSSA
 
     return ret;
   }
-
-  /*
-  CoefContainer::CoefContainer(const std::string spec)
-  {
-    YAML::Node top = YAML::LoadFile(spec);
-
-    // Defaults
-    tmin   = -std::numeric_limits<double>::max();
-    tmax   =  std::numeric_limits<double>::max();
-    runtag = "mssa";
-    stride = 1;
-
-    // Overrides
-    if (top["tmin"])   tmin   = top["tmin"].as<double>();
-    if (top["tmax"])   tmax   = top["tmax"].as<double>();
-    if (top["stride"]) stride = top["stride"].as<int>();
-    if (top["runtag"]) runtag = top["runtag"].as<std::string>();
-
-    if (top["components"]) {
-
-      YAML::Node node = top["components"];
-
-      for (YAML::const_iterator it=node.begin(); it!=node.end(); it++) {
-	std::string name = it->first.as<std::string>();
-	YAML::Node node = it->second;
-	std::string ctype("sphere");
-	if (node["geometry"]) {
-	  ctype = node["geometry"].as<std::string>();
-	  std:: transform(ctype.begin(), ctype.end(), ctype.begin(),
-			  [](unsigned char c){ return std::tolower(c);});
-	} else {
-	  std::cout << "Geometry not specified.  You must specify 'cylinder' or 'sphere'." << std::endl;
-	  exit(-1);
-	}
-
-	// Create the instances
-	//
-	if (ctype.find("sphere")==0) {
-	  namemap[name] = comps.size();
-	  comps.push_back(std::make_shared<SphDB>(name, node, comps.size(), stride, tmin, tmax));
-	} else if (ctype.find("cylinder")==0) {
-	  namemap[name] = comps.size();
-	  comps.push_back(std::make_shared<CylDB>(name, node, comps.size(), stride, tmin, tmax));
-	} else if (ctype.find("table")==0) {
-	  namemap[name] = comps.size();
-	  comps.push_back(std::make_shared<TableDB>(name, node, comps.size(), stride, tmin, tmax));
-	} else {
-	  std::cout << "Unknown geometry.  You must specify 'cylinder', 'sphere', or 'table'." << std::endl;
-	  exit(-1);
-	}
-      }
-    } else {
-      std::cout << "CoefContainer: no components specified" << std::endl;
-      exit(-1);
-    }
-
-    // Check times (all should be the same)
-    //
-				// First check lengths
-    size_t tsize = comps[0]->times.size();
-    for (size_t n=1; n<comps.size(); n++) {
-      if (tsize != comps[n]->times.size()) {
-	std::cout << "CoefContainer: times lengths do not agree!" << std::endl;
-	exit(-4);
-      }
-				// Now check time values
-      for (size_t t=0; t<tsize; t++) {
-	if (fabs(comps[0]->times[t] - comps[n]->times[t]) > 1.0e-8) {
-	  std::cout << "CoefContainer: times disagree for indices 0 and "
-		    << n << std::endl;
-	  exit(-5);
-	}
-      }
-    }
-
-    // Waste a little space for convenience
-    //
-    times = comps[0]->times;
-
-    // Make key list
-    //
-    for (size_t n=0; n<comps.size(); n++) {
-      for (size_t k=0; k<comps[n]->keys.size(); k++) {
-	std::vector<unsigned> keyp = comps[n]->keys[k];
-	keyp.push_back(comps[n]->index);
-	keylist.push_back(keyp);
-      }
-    }
-  }
-  */
 
   CoefContainer::CoefContainer(const mssaConfig& config, const std::string spec)
   {
