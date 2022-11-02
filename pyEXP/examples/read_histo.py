1342bdf5a1be5f84784558fd7fd7da7f306eea2f
Martin D. Weinberg <weinberg@astro.umass.edu>
2022-11-01 20:16:23 -0400

    Added file name input

--- pyEXP/examples/read_histo.py
+++ pyEXP/examples/read_histo.py
@@ -3,7 +3,11 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
-file = open('imagePickle', 'rb')
+if not os.path.exists(sys.argv[1]):
+    print("File <{}> does not exist?".format(sys.argv[1]))
+    exit(1)
+
+file = open(sys.argv[1], 'rb')
 db = pickle.load(file)
 file.close()
 
