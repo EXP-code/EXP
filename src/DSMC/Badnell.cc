8c3b3acb5d7c05bc46c3146e7e05ed04e7264953
Martin D. Weinberg <weinberg@astro.umass.edu>
2022-12-21 16:57:11 -0500

    Updates for Nigel's new DR cross sections

--- src/DSMC/Badnell.cc
+++ src/DSMC/Badnell.cc
@@ -128,8 +128,9 @@ BadnellData::walkDirectory()
 
 void BadnellData::initialize(atomicData* ad)
 {
-  // Attempt to open files for ions in ionQ
+  // Periodic table
   //
+  PeriodicTable table;
 
   /** From Nigel
 
@@ -143,10 +144,6 @@ void BadnellData::initialize(atomicData* ad)
       coeffs.
   */
 
-  // Filter strings
-  //
-  const std::string user("nrb"), year("20");
-
   // Set default datapath
   //
   std::filesystem::path dpath{datapath};
@@ -157,62 +154,169 @@ void BadnellData::initialize(atomicData* ad)
     dpath = std::filesystem::path( env_p );
   }
   
-  // Loop through ion list and read data
+  // Filter strings
+  //
+  const std::string user("nrb"), year("20"); // RR 
+
+
+  // File regular expression for RR
+  //
+  std::ostringstream rstr;
+
+  // Path--+               Sequence-----+     +---Separator
+  //       |                            |     |
+  //       |               Separator--+ |     |
+  //       |                          | |     |
+  //       v                          v v     v
+  rstr << ".*[/]" << user << year << "#[a-z]+[_]"
+       << "([a-z]+)([0-9]+)" << coupling << "[0-9]*[.]dat";
+  //        ^       ^           ^            ^        ^
+  //        |       |           |            |        |
+  //        |       |           |            +--------|----- Core excitation
+  //        |       |           |                     |      n->n' for
+  // Ion name       Charge      Coupling     Suffix---+      dielectronic
+  // (Group 1)      (Group 2)
+
+  std::regex species(rstr.str());
+  std::smatch matches;
+
+  // Will contain matched file(s).  Should only be one.
+  //
+  std::map<lQ, std::string> names;
+
+  try {
+    for (auto const& dir_entry : std::filesystem::recursive_directory_iterator{dpath / "RR"}) {
+      std::string name = dir_entry.path().string();
+      if (std::regex_search(name, matches, species)) {
+	auto it = table[matches[1].str()];
+	if (it == 0) {
+	  std::ostringstream sout;
+	  sout << "Badnell::initialize: [0] could not locate element <"
+	       << matches[1].str() << ">";
+	  throw std::runtime_error(sout.str());
+	}
+
+	// The element and and charge
+	//
+	lQ ZC = {it->number(), stoi(matches[2].str()) + 1};
+
+	// Add the file name
+	//
+	names[ZC] = name;
+      }
+    }
+  }
+  catch(std::filesystem::filesystem_error const& ex) {
+    std::cout
+      << "Directory scan error for RR..."
+      << "what():  " << ex.what() << '\n'
+      << "path1(): " << ex.path1() << '\n'
+      << "path2(): " << ex.path2() << '\n'
+      << "code().value():    " << ex.code().value() << '\n'
+      << "code().message():  " << ex.code().message() << '\n'
+      << "code().category(): " << ex.code().category().name() << '\n';
+    
+    MPI_Finalize();
+    exit(32);
+  }
+    
+  // File regular expressions for DR
+  //
+  std::ostringstream rdr1, rdr2;
+  std::string yr1("00"), yr2("12");
+
+  // Path--+              Sequence-----+     +---Separator
+  //       |                           |     |
+  //       |              Separator--+ |     |
+  //       |                         | |     |
+  //       v                         v v     v
+  rdr1 << ".*[/]" << user << yr1 << "#[a-z]+[_]"
+       << "([a-z]+)([0-9]+)" << coupling << "([0-9]*)_t5.dat";
+  rdr2 << ".*[/]" << user << yr2 << "#[a-z]+[_]"
+       << "([a-z]+)([0-9]+)" << coupling << "([0-9]*)_t5.dat";
+  //        ^       ^           ^             ^          ^
+  //        |       |           |             |          |
+  //        |       |           |             +----------|-- Core excitation
+  //        |       |           |                        |   n->n' for
+  // Ion name       Charge      Coupling        Suffix---+   dielectronic
+  // (Group 1)      (Group 2)                                (Group 3)
+
+  std::regex spc1(rdr1.str());
+  std::regex spc2(rdr2.str());
+
+  std::smatch match1, match2;
+
+  std::map<lQ, std::vector<std::pair<std::string, std::string>>> names2;
+
+  try {
+    for (auto const& dir_entry : std::filesystem::recursive_directory_iterator{dpath / "DR"}) {
+      std::string name = dir_entry.path().string();
+      if (std::regex_search(name, match1, spc1)) {
+	auto it = table[match1[1].str()];
+	if (it == 0) {
+	  std::ostringstream sout;
+	  sout << "Badnell::initialize: [1] could not locate element <"
+	       << match1[1].str() << ">";
+	  throw std::runtime_error(sout.str());
+	}
+
+	// The element and and charge
+	//
+	lQ ZC = {it->number(), stoi(match1[2].str()) + 1};
+
+	// Add this excitaton to the db
+	//
+	names2[ZC].push_back({match1[3].str(), name});
+      }
+      if (std::regex_search(name, match2, spc2)) {
+	auto it = table[match2[1].str()];
+	if (it == 0) {
+	  std::ostringstream sout;
+	  sout << "Badnell::initialize: [2] could not locate element <"
+	       << match2[1].str() << ">";
+	  throw std::runtime_error(sout.str());
+	}
+
+	// The element and and charge
+	//
+	lQ ZC = {it->number(), stoi(match2[2].str()) + 1};
+
+	// Add this excitaton to the db
+	//
+	names2[ZC].push_back({match1[3].str(), name});
+      }
+    }
+  }
+  catch(std::filesystem::filesystem_error const& ex) {
+    std::cout
+      << "Directory scan error for DR..."
+      << "what():  " << ex.what() << '\n'
+      << "path1(): " << ex.path1() << '\n'
+      << "path2(): " << ex.path2() << '\n'
+      << "code().value():    " << ex.code().value() << '\n'
+      << "code().message():  " << ex.code().message() << '\n'
+      << "code().category(): " << ex.code().category().name() << '\n';
+    
+    MPI_Finalize();
+    exit(34);
+  }
+
+  // Loop through each desired ion and process the files
   //
   for (auto ZC : ionQ) {
 
-    // File regular expression
+    // Begin with RR files
     //
-    std::ostringstream rstr;
+    auto it = names.find(ZC);
 
-    // Path--+               Sequence-----+     +---Separator
-    //       |                            |     |
-    //       |               Separator--+ |     |           +--Core excitation
-    //       |                          | |     |           |  n->n' for
-    //       v                          v v     v           |  dielectronic
-    rstr << ".*[/]" << user << year << "#[a-z]+[_]" //      v         
-	 << ions[ZC.first] << ZC.second - 1 << coupling << "[0-9]*[.]dat";
-    //        ^               ^                ^                     ^
-    //        |               |                |                     |
-    // Ion name               Charge           Coupling     Suffix---+
-
-    std::regex species(rstr.str());
-    std::smatch matches;
-
-    // Will contain matched file(s).  Should only be one.
-    //
-    std::vector<std::string> names;
-
-    try {
-      for (auto const& dir_entry : std::filesystem::recursive_directory_iterator{dpath / "RR"}) {
-	std::string name = dir_entry.path().string();
-	if (std::regex_search(name, matches, species))
-	  names.push_back(name);
-      }
-    }
-    catch(std::filesystem::filesystem_error const& ex) {
-      std::cout
-	<< "Directory scan error for RR..."
-	<< "what():  " << ex.what() << '\n'
-	<< "path1(): " << ex.path1() << '\n'
-	<< "path2(): " << ex.path2() << '\n'
-	<< "code().value():    " << ex.code().value() << '\n'
-	<< "code().message():  " << ex.code().message() << '\n'
-	<< "code().category(): " << ex.code().category().name() << '\n';
-      
-      MPI_Finalize();
-      exit(32);
-    }
-    
-    if (names.size() != 1) {
-      std::cout << "Unexpected RR matches: " << names.size() << std::endl;
-      for (auto s : names) std::cout << s << " ";
-      std::cout << std::endl;
-      MPI_Finalize();
-      exit(33);
+    if (it == names.end()) {
+      std::ostringstream sout;
+      sout << "Badnell::initialize: could not locate RR file for ion (Z, C) = ("
+	   << ZC.first << ", " << ZC.second << ")";
+      throw std::runtime_error(sout.str());
     }
 
-    std::ifstream in{dpath / names[0]};
+    std::ifstream in{dpath / it->second};
     
     std::string line;
     std::getline(in, line);
@@ -252,96 +356,121 @@ void BadnellData::initialize(atomicData* ad)
     }
     // END: RR file read
     
-    // Now read DR
+    // Now, on to DR files
     //
+    auto it2 = names2.find(ZC);
 
-    names.clear();
-    try {
-      for (auto const& dir_entry : std::filesystem::directory_iterator{dpath / "DR"}) {
-	std::string name = dir_entry.path().string();
-	if (std::regex_search(name, matches, species)) {
-	  names.push_back(name);
-	}
-      }
-    }
-    catch(std::filesystem::filesystem_error const& ex) {
-      std::cout
-	<< "Directory scan error for DR..."
-	<< "what():  " << ex.what() << '\n'
-	<< "path1(): " << ex.path1() << '\n'
-	<< "path2(): " << ex.path2() << '\n'
-	<< "code().value():    " << ex.code().value() << '\n'
-	<< "code().message():  " << ex.code().message() << '\n'
-	<< "code().category(): " << ex.code().category().name() << '\n';
+    if (it2 != names2.end()) {
       
-      MPI_Finalize();
-      exit(34);
-    }
+      for (auto v : it2->second) {
 
-    if (names.size()) {
+	auto cr = v.first;
+	// TEST
+	if (cr.size()>2) continue;
 
-      if (names.size() != 1) {
-	std::cout << "Unexpected DR matches: " << names.size() << std::endl;
-	for (auto s : names) std::cout << s << " ";
-	std::cout << std::endl;
-	MPI_Finalize();
-	exit(35);
-      }
-
-      in = std::ifstream{dpath / "DR" / names[0]};
-      
-      std::getline(in, line);
-    
-      looking = true;
-      while (in) {
-	if (looking) {
-	  // Keep reading lines until we reach the totals
-	  //
-	  if (line.find("E(RYD)") != std::string::npos) {
-	    looking = false;
-	    std::getline(in, line); // Read header
-	  }
-	} else {
-	  // We are done!
-	  //
-	  if (line[0] == 'C') break;
-	  
-	  // Read the line
-	  //
-	  std::istringstream ins(line);
-
-	  // Default values
-	  //
-	  E = X = 0.0;
-
-	  // Values (Energy in Rydberg and energy averaged cross
-	  // section in Mbarn)
-	  ins >> E >> X;
-	  d->E_dr.push_back(E*RydtoeV); // Energy is now in eV
-	  d->X_dr.push_back(X);
-	}
+	in = std::ifstream{dpath / "DR" / v.second};
 	
-	// Read next line
 	std::getline(in, line);
-      }
-      // END: DR file read
-
-      // Reweight energy average
-      //
-      if (reweight) {
-	for (int i=0; i<d->E_dr.size()-1; i++) {
-	  if (i<d->X_dr.size())
-	    d->X_dr[i] *= 2.0/(1.0 + d->E_dr[i]/d->E_dr[i+1]);
-	  else
-	    std::cout << "DR error for [" << ZC.first << ", " << ZC.second
-		      << "]: i=" << i << " !< " << d->X_dr.size() << std::endl;
+	
+	looking = true;
+	while (in) {
+	  if (looking) {
+	    // Keep reading lines until we reach the totals
+	    //
+	    if (line.find("E(RYD)") != std::string::npos) {
+	      looking = false;
+	      std::getline(in, line); // Read header
+	    }
+	  } else {
+	    // We are done!
+	    //
+	    if (line[0] == 'C') break;
+	    
+	    // Read the line
+	    //
+	    std::istringstream ins(line);
+	    
+	    // Default values
+	    //
+	    E = X = 0.0;
+	    
+	    // Values (Energy in Rydberg and energy averaged cross
+	    // section in Mbarn)
+	    ins >> E >> X;
+	    d->E_dr[cr].push_back(E*RydtoeV); // Energy is now in eV
+	    d->X_dr[cr].push_back(X);
+	  }
+	
+	  // Read next line
+	  std::getline(in, line);
 	}
-      } // END: reweight
-      
-    } // END: DR will not exist for hydrogenic case
+	// END: DR file read
+	
+	// Reweight energy average
+	//
+	if (reweight) {
+	  for (int i=0; i<d->E_dr[cr].size()-1; i++) {
+	    if (i<d->X_dr[cr].size())
+	      d->X_dr[cr][i] *= 2.0/(1.0 + d->E_dr[cr][i]/d->E_dr[cr][i+1]);
+	    else
+	      std::cout << "DR error for [" << ZC.first << ", " << ZC.second
+			<< "]: i=" << i << " !< " << d->X_dr[cr].size() << std::endl;
+	  }
+	}
+	// END: reweight
+	
+      }
+      // END: DR stanza
+
+      //       +--- Verbose reporting on DR files found
+      //       |    (not for production mode)
+      //       v
+    } else if (false) {
+      std::ostringstream sout;
+      sout << "Badnell::initialize: could not locate DR file for element "
+	   << table[ZC.first]->name() << ", ion (Z, C) = ("
+	   << ZC.first << ", " << ZC.second << ")";
+      std::cout << sout.str() << std::endl;
+    }
+    // END: file loop
 
     data[ZC] = d;
   }
 
   // DONE
 }
+
+void BadnellData::report()
+{
+  std::cout << std::string(64, '-') << std::endl
+	    << "ADAS cross section check" << std::endl
+	    << std::string(64, '-') << std::endl
+	    << std::setw(8) << "Z"
+	    << std::setw(8) << "C"
+	    << std::setw(8) << "N(RR)"
+	    << std::setw(8) << "core"
+	    << std::setw(8) << "N(DR)"
+	    << std::endl
+	    << std::setw(8) << "-----"
+	    << std::setw(8) << "-----"
+	    << std::setw(8) << "-----"
+	    << std::setw(8) << "-----"
+	    << std::setw(8) << "-----"
+	    << std::endl;
+
+  for (auto d : data) {
+    std::cout << std::setw(8) << d.first.first
+	      << std::setw(8) << d.first.second
+	      << std::setw(8) << d.second->E_rr.size()
+	      << std::endl;
+    for (auto s : d.second->E_dr) {
+      std::cout << std::setw(8) << ""
+		<< std::setw(8) << ""
+		<< std::setw(8) << ""
+		<< std::setw(8) << s.first
+		<< std::setw(8) << s.second.size() << std::endl;
+    }
+  }
+  std::cout << std::string(32, '-') << std::endl;
+}
+
