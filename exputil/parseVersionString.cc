f1f3e2d8c4a196daca350f8f8890434c554da27f
Martin D. Weinberg <weinberg@astro.umass.edu>
2022-12-24 09:51:48 -0500

    Add version string parsing utility: takes a string and returns a vector of major, minor, subminor, ... integer values

---
+++ exputil/parseVersionString.cc
@@ -0,0 +1,22 @@
+#include <parseVersionString.H>
+
+std::vector<int> parseVersionString(const std::string& version_str)
+{
+  std::vector<int> version;
+  size_t pos = 0;
+
+  while (pos<version_str.size()) {
+
+    size_t dot_pos = version_str.find('.', pos);
+    size_t n = dot_pos;
+    if (dot_pos != std::string::npos) n = dot_pos - pos;
+
+    version.push_back(atoi(version_str.substr(pos, n).c_str()));
+
+    if (dot_pos == std::string::npos) return version;
+
+    pos = dot_pos + 1;
+  }
+
+  return version;
+}
