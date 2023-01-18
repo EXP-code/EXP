f1fc390f216f34866b41da48699a946a67b1c531
Martin D. Weinberg <weinberg@astro.umass.edu>
2023-01-18 02:21:55 -0500

    Added Bessel function tests

--- exputil/bessz.cc
+++ exputil/bessz.cc
@@ -1,51 +1,44 @@
 /**************************************************************************
 *
-*		Computes m zeros of spherical Bessel functions (first kind) 
-*               j_n(x) by brute force and using Watson's relation for the
-*               location of the first zero
+*		Computes m zeros of spherical Bessel functions (first
+*               kind) j_n(x) by brute force and using Watson's
+*               relation for the location of the first zero
 *
 *
 *		MDW: 12/26/1987
 *                    port to C++ 02/15/94
+*                    updated for C++17 01/11/22
 **************************************************************************/
 
 #include <cmath>
 #include <numerical.H>
 
-#define STEPS 40
-#define TOL 1.0e-10
-
-static int NN;
-
-static double zbess(double z)
-{
-  return jn(NN, z)*NN/z - jn(NN+1, z);
-}
+#define STEPS 6
+#define TOL 1.0e-7
 
 Eigen::VectorXd bessjz(int n, int m)
 {
-  double z,dz,zl,f,fl;
-
   Eigen::VectorXd a(m);
 
-  NN = n;
-  dz = M_PI/STEPS;
-  for (int i=0, zl=z=1.0e-8, fl=zbess(z); i<m; i++) {
+  auto zfunc = [n](double z) { return std::cyl_bessel_j(n, z); };
+
+  double dz = M_PI/STEPS;
+  double z  = 0.5+fabs((double)n), zl, fl, f;
+
+  for (int i=0; i<m; i++) {
+    fl = std::cyl_bessel_j(n, z);
     z += dz;
-    f = zbess(z);
-    while (f*fl>0) {
+    f  = std::cyl_bessel_j(n, z);
+    while (f*fl>0.0) {
       zl = z;
       fl = f;
       z += dz;
-      f = zbess(z);
+      f = std::cyl_bessel_j(n,z);
     }
-    a[i] = zbrent(zbess,zl,z,TOL);
-    z = a[i];
-    zl = a[i] + 100.0*TOL;
-    fl = zbess(zl);
+    a[i] = zbrent(zfunc, zl, z, TOL);
+    zl = z;
+    fl = f;
   }
 
   return a;
-
 }
-
