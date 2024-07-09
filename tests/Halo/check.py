b2041ad709e3ef4f694b31ad64d8e556a38bc7f6
Martin D. Weinberg <weinberg@astro.umass.edu>
2024-07-09 08:53:19 -0700

    A few minor updates and clean ups [no ci]

--- tests/Halo/check.py
+++ tests/Halo/check.py
@@ -3,15 +3,12 @@ import numpy as np
 # Read the output log file
 data = np.loadtxt("OUTLOG.run0", skiprows=6, delimiter="|")
 
-# Column 16 is -2T/VC.  The mean should be 1 with some small number of std dev
+# Column 16 is -2T/VC.  The mean should be 1
 mean = np.mean(data[:,16])
 stdv = np.std (data[:,16])
 
-# print("Mean = " + str(mean) + ", std dev = " + str(stdv))
-# print("Check = {}".format(np.abs(mean-1.0) - 5.0*stdv))
-
-# If the values are within 3 sigma, assume that the simulation worked
-if np.abs(mean - 1.0) > 5.0*stdv:
+# If the values are within 6 sigma of 1, assume that the simulation worked
+if np.abs(mean - 1.0) > 6.0*stdv:
     exit(1)
 else:
     exit(0)
