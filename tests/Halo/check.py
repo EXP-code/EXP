23fbb21334233f203f5616acd21eb26d846a5916
Martin D. Weinberg <weinberg@astro.umass.edu>
2024-07-08 23:25:21 -0700

    Updates to 'make tests' scripts and config [no ci]

--- tests/Halo/check.py
+++ tests/Halo/check.py
@@ -3,12 +3,15 @@ import numpy as np
 # Read the output log file
 data = np.loadtxt("OUTLOG.run0", skiprows=6, delimiter="|")
 
-# Column 16 is -2T/VC.  The mean should be 1 with a std dev < 0.03
+# Column 16 is -2T/VC.  The mean should be 1 with some small number of std dev
 mean = np.mean(data[:,16])
 stdv = np.std (data[:,16])
 
+# print("Mean = " + str(mean) + ", std dev = " + str(stdv))
+# print("Check = {}".format(np.abs(mean-1.0) - 5.0*stdv))
+
 # If the values are within 3 sigma, assume that the simulation worked
-if np.abs(mean - 1.0) > 3.0*stdv:
+if np.abs(mean - 1.0) > 5.0*stdv:
     exit(1)
 else:
     exit(0)
