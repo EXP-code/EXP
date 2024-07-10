65dd03aabe4e38a1b2f6cc152cf9fa9f488c185f
Martin D. Weinberg <weinberg@astro.umass.edu>
2024-07-09 21:05:25 -0700

    Remove numpy dependence from Cube OUTLOG check [no ci]

--- tests/Cube/check.py
+++ tests/Cube/check.py
@@ -1,15 +1,26 @@
-import numpy as np
+# open the output log file
+file = open("OUTLOG.runS")
 
-# Read the output log file
-data = np.loadtxt("OUTLOG.runS", skiprows=6, delimiter="|")
+n = 0                           # Count lines
+mean = [0.0, 0.0, 0.0]          # Mean positions
 
-# Columns 4, 5, 6 is mean position
-x = np.mean(data[:,3])
-y = np.mean(data[:,4])
-z = np.mean(data[:,5])
+# Open the output log file
+#
+while (line := file.readline()) != "":
+    if n >= 6:                  # Skip the header stuff
+        v = [float(x) for x in line.split('|')]
+        mean[0] += v[3]         # x pos
+        mean[1] += v[4]         # y pos
+        mean[2] += v[5]         # z pos
+    n = n + 1                   # Count lines
 
-# If the values are close to 0.5, assume it worked
-if np.abs(x - 0.5) > 0.15 or np.abs(y - 0.5) > 0.15 or np.abs(z - 0.5) > 0.15:
+if n>6:                         # Sanity check
+    for i in range(3):
+        mean[i] = mean[i]/(n-6) - 0.5
+
+# If the squared values are close to 0.0, assume it worked
+#
+if mean[0]*mean[0] > 0.03 or mean[1]*mean[1] > 0.03 or mean[2]*mean[2] > 0.03:
     exit(1)
 else:
     exit(0)
