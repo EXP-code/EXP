4c272053083475fd26d5a910805b8de449379ce3
Martin Weinberg <mdw@umass.edu>
2024-10-13 11:37:29 -0400

    Update createCoefs.py
    One more try just to make sure that this fails on all `createFromArrry` statements.

--- tests/Halo/createCoefs.py
+++ tests/Halo/createCoefs.py
@@ -47,6 +47,8 @@ for i in range(0, 100):
 print("---- createFromArray usings lists")
 coef1 = basis.createFromArray(mass, [xpos, ypos, zpos], time=3.0)
 
+exit(0) # TEST
+
 mass  = np.array(mass)
 data  = np.array([xpos, ypos, zpos])
 
