86c8c4e74e925089e9066c6e721f9e5fb581a0c6
Martin D. Weinberg <weinberg@astro.umass.edu>
2024-12-25 16:11:54 -0500

    Allow the function name to be user settable, although we are not currently using this feature

--- exputil/DiskDensityFunc.cc
+++ exputil/DiskDensityFunc.cc
@@ -2,20 +2,26 @@
 
 namespace py = pybind11;
 
-DiskDensityFunc::DiskDensityFunc(const std::string& modulename)
+DiskDensityFunc::DiskDensityFunc(const std::string& modulename,
+				 const std::string& funcname)
+  : funcname(funcname)
 {
+  // Check if a Python interpreter exists
   if (Py_IsInitialized() == 0) {
     py::initialize_interpreter();
+    // Mark the interpreter as started by this instance
     started = true;
   }
 
+  // Bind the disk_density function from Python
   disk_density =
     py::reinterpret_borrow<py::function>
-    ( py::module::import(modulename.c_str()).attr("disk_density") );
+    ( py::module::import(modulename.c_str()).attr(funcname.c_str()) );
 }
 
 DiskDensityFunc::~DiskDensityFunc()
 {
+  // Only end the interpreter if it was started by this instance
   if (started) py::finalize_interpreter();
 }
 
