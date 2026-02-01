0a507cd18d19282591af2e82644fb6f0f73e0056
Martin D. Weinberg <weinberg@astro.umass.edu>
2026-02-01 18:36:10 -0500

    Change from 'afac' to 'bias' to be a bit more descriptive

--- src/Cylinder.cc
+++ src/Cylinder.cc
@@ -28,7 +28,7 @@ Cylinder::valid_keys = {
   "rcylmin",
   "rcylmax",
   "acyl",
-  "afac",
+  "bias",
   "hcyl",
   "sech2",
   "hexp",
@@ -105,7 +105,7 @@ Cylinder::Cylinder(Component* c0, const YAML::Node& conf, MixtureBasis *m) :
   ncylr           = 2000;
 
   acyl            = 0.01;
-  afac            = 1.0;
+  bias            = 1.0;
   lmaxfid         = 128;
   nmaxfid         = 64;
   mmax            = 6;
@@ -178,7 +178,7 @@ Cylinder::Cylinder(Component* c0, const YAML::Node& conf, MixtureBasis *m) :
   // Make the empirical orthogonal basis instance
   //
   ortho = std::make_shared<CylEXP>
-    (nmaxfid, lmaxfid, mmax, nmax, afac*acyl, hcyl, ncylodd, cachename);
+    (nmaxfid, lmaxfid, mmax, nmax, bias*acyl, hcyl, ncylodd, cachename);
   
   // Set azimuthal harmonic order restriction?
   //
@@ -334,7 +334,7 @@ Cylinder::Cylinder(Component* c0, const YAML::Node& conf, MixtureBasis *m) :
 		<< std::endl << sep << "rcylmin="     << rcylmin
 		<< std::endl << sep << "rcylmax="     << rcylmax
 		<< std::endl << sep << "acyl="        << acyl
-		<< std::endl << sep << "afac="        << afac
+		<< std::endl << sep << "bias="        << bias
 		<< std::endl << sep << "hcyl="        << hcyl
 		<< std::endl << sep << "precond="     << precond
 		<< std::endl << sep << "pcavar="      << pcavar
@@ -364,7 +364,7 @@ Cylinder::Cylinder(Component* c0, const YAML::Node& conf, MixtureBasis *m) :
 	      << std::endl << sep << "rcylmin="     << rcylmin
 	      << std::endl << sep << "rcylmax="     << rcylmax
 	      << std::endl << sep << "acyl="        << acyl
-	      << std::endl << sep << "afac="        << afac
+	      << std::endl << sep << "bias="        << bias
 	      << std::endl << sep << "hcyl="        << hcyl
 	      << std::endl << sep << "precond="     << precond
 	      << std::endl << sep << "pcavar="      << pcavar
@@ -421,7 +421,7 @@ void Cylinder::initialize()
     if (conf["rcylmax"   ])    rcylmax  = conf["rcylmax"   ].as<double>();
 
     if (conf["acyl"      ])       acyl  = conf["acyl"      ].as<double>();
-    if (conf["afac"      ])       afac  = conf["afac"      ].as<double>();
+    if (conf["bias"      ])       bias  = conf["bias"      ].as<double>();
     if (conf["hcyl"      ])       hcyl  = conf["hcyl"      ].as<double>();
     if (conf["sech2"     ])      sech2  = conf["sech2"     ].as<bool>();
     if (conf["hexp"      ])       hexp  = conf["hexp"      ].as<double>();
