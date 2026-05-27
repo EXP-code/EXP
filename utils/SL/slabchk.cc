dba16fac54eb88b04b195419062299799cbaf70f
Martin D. Weinberg <weinberg@astro.umass.edu>
2026-05-27 08:44:04 -0400

    Add the Uniform density model

--- utils/SL/slabchk.cc
+++ utils/SL/slabchk.cc
@@ -13,31 +13,37 @@
 int 
 main(int argc, char** argv)
 {
-  double H, zmax, zend;
-  int kmax, nmax, knots, numz;
-  std::string filename;
+  double H, zmax, zend, sigma;
+  int kx, nmax, knots, numz, nfid;
+  std::string model, filename;
 
   // Parse command line
   //
-  cxxopts::Options options(argv[0], "Check the consistency a spherical SL basis");
+  cxxopts::Options options(argv[0], "Check the consistency a slab SL basis");
 
   options.add_options()
     ("h,help", "Print this help message")
-    ("ortho", "Compute orthogonality matrix")
+    ("o,ortho", "Compute orthogonality matrix")
     ("H,height", "Slab scale height",
      cxxopts::value<double>(H)->default_value("1.0"))
-    ("K,kmax", "maximum order of in-plane harmonics",
-     cxxopts::value<int>(kmax)->default_value("4"))
+    ("K,kx", "order of in-plane harmonics",
+     cxxopts::value<int>(kx)->default_value("0"))
     ("N,nmax", "maximum number of vertical harmonics",
      cxxopts::value<int>(nmax)->default_value("18"))
     ("n,numz", "size of vertical grid",
      cxxopts::value<int>(numz)->default_value("1000"))
     ("Z,zmax", "maximum extent of vertical grid",
      cxxopts::value<double>(zmax)->default_value("10.0"))
-    ("zend",   "potential offset",
+    ("s,sigma","Velocity dispersion for model",
+     cxxopts::value<double>(sigma)->default_value("1.0"))
+    ("d,zend",   "potential offset",
      cxxopts::value<double>(zend)->default_value("0.0"))
-    ("knots", "Number of Legendre integration knots",
+    ("k,knots", "Number of Legendre integration knots",
      cxxopts::value<int>(knots)->default_value("40"))
+    ("j,ncheck", "Order of coefficient to check for orthogonality",
+     cxxopts::value<int>(nfid)->default_value("2"))
+    ("m,model", "Model type (Sech2mu, Uniform)",
+     cxxopts::value<std::string>(model)->default_value("Sech2mu"))
     ("p,prefix", "Output filename prefix",
      cxxopts::value<std::string>(filename)->default_value("slabchk_test"))
     ;
@@ -63,17 +69,34 @@ main(int argc, char** argv)
     return 1;
   }
 
-  // Generate Sech2 disk grid
+  // Define model type and instantiate model
   //
-  double dispz = 2.0*M_PI*H*H;
+  OneDModel::ModelType mtype = OneDModel::parse_model_type(model);
+  std::shared_ptr<OneDModel> mdl;
+  std::string mname = "isothermal";
+  
+  if (mtype == OneDModel::ModelType::uniform) {
+    
+    std::cout << "Harmonic potential: setting scale height to "
+	      << H << " and Zmax to " << H*0.999 << std::endl;
+    mdl = std::make_shared<Uniform>(H, sigma, sigma);
+    zmax  = H*0.999;
+    mname = "uniform";
+  } else if (mtype == OneDModel::ModelType::sech2mu) {
+    
+    std::cout << "Sech2 potential: using scale height " << H << std::endl;
 
-  Sech2  sech2(dispz);
-  double h = sech2.get_scale_height();
-
-  SLGridSlab::H = h;
-  SLGridSlab::ZEND = zend;
-  std::cout << "Check...scale height is: " <<  SLGridSlab::H 
-	    << std::endl << std::endl;
+    auto tmp = std::make_shared<Sech2mu>(sigma*sigma, H);
+    tmp->set_hmax(10.0*H);
+    mdl = tmp;
+    
+    SLGridSlab::H = H;
+    SLGridSlab::ZEND = zend;
+    mname = "sech2mu";
+  } else {
+    std::cerr << "model must be 'Sech2mu' or 'Harmonic'\n";
+    return 1;
+  }
 
   // Particle position generator
   //
@@ -81,17 +104,23 @@ main(int argc, char** argv)
   std::mt19937 gen(rd());
   std::uniform_real_distribution<> dis(0.0, 1.0);
 
-  auto sample = [&dis, &gen, &h]()
+  auto sample = [&dis, &gen, &H, &mtype]()
   {
-    double m = dis(gen);
-    return 0.5*h*log(m/(1.0 - m));
+    if (mtype == OneDModel::ModelType::uniform) {
+      return H*(2.0*dis(gen) - 1.0);
+    } else if (mtype == OneDModel::ModelType::sech2mu) {
+      double m = dis(gen);
+      return 0.5*H*log(m/(1.0 - m));
+    } else {
+      throw std::runtime_error("Invalid model type for sampling");
+    }
   };
 
   // Generate Sturm-Liouville grid
   //
-  auto ortho = std::make_shared<SLGridSlab>(kmax, nmax, numz, zmax,
+  auto ortho = std::make_shared<SLGridSlab>(kx+1, nmax, numz, zmax,
 					    ".slgrid_slab_cache",
-					    "linear", "isothermal");
+					    "linear", mname, false);
 
   LegeQuad lw(knots);
 	  
@@ -107,12 +136,12 @@ main(int argc, char** argv)
 
     for (int n=0; n<nmax; n++) {
 	      
-      ans1[n] += -ortho->get_pot(x, 0, 0, n, 0)*
-	ortho->get_dens(x, 0, 0, 1, 0) /
+      ans1[n] += -ortho->get_pot(x, kx, 0, n, 0)*
+	ortho->get_dens(x, 0, 0, nfid, 0) /
 	ortho->d_xi_to_z(x) * (ximax - ximin)*lw.weight(i);
     
-      ans2[n] += -ortho->get_pot(x, 0, 0, n, 0)*
-	4.0*M_PI*sech2.get_density(z) /
+      ans2[n] += -ortho->get_pot(x, kx, 0, n, 0)*
+	4.0*M_PI*mdl->get_density(z) /
 	ortho->d_xi_to_z(x) * (ximax - ximin)*lw.weight(i);
     }
   }
@@ -120,7 +149,7 @@ main(int argc, char** argv)
   // Monte Carlo version
   //
   int Number = 100000;
-  double fac = 4.0*M_PI*2.0*h/Number;
+  double fac = 4.0*M_PI/Number;
 
   for (int i=0; i<Number; i++) {
 	    
@@ -128,23 +157,41 @@ main(int argc, char** argv)
     double x = ortho->z_to_xi(z);
 
     for (int n=0; n<nmax; n++) {
-      ans3[n] += -ortho->get_pot(x, 0, 0, n, 0) * fac;
+      ans3[n] += -ortho->get_pot(x, kx, 0, n, 0) * fac;
     }
   }
     
+  std::cout << "---- Coefficients\n";
+  std::cout << std::setw(6)  << "n"
+	    << std::setw(18) << "Dens(1)"
+	    << std::setw(18) << "Dens(model)"
+	    << std::setw(18) << "Dens(MC)"
+	    << std::setw(18) << "Model dens at z=0"
+	    << std::endl;
+
   for (int n=0; n<nmax; n++) {
     std::cout << std::setw(6)  << n
 	      << std::setw(18) << ans1[n]
 	      << std::setw(18) << ans2[n]
 	      << std::setw(18) << ans3[n]
-	      << std::setw(18) << ans2[n]*ortho->get_dens(0.0, 0, 0, n)/(4.0*M_PI)
+	      << std::setw(18) << ans2[n]*ortho->get_dens(0.0, kx, 0, n)/(4.0*M_PI)
 	      << std::endl;
   }
   
   std::cout << std::endl;
 
   int NUM = 20;
-  double dz = 3.0*H/(NUM-1);
+  double dz = zmax/(NUM-1);
+
+  std::cout << "---- Comparing SL solution to model . . .\n";
+  std::cout << std::setw(16) << "z"
+	    << std::setw(16) << "rho (SL)"
+	    << std::setw(16) << "rho (mod)"
+	    << std::setw(16) << "phi (SL)"
+	    << std::setw(16) << "phi (mod)"
+	    << std::setw(16) << "dphi (SL)"
+	    << std::setw(16) << "dphi (mod)"
+	    << std::endl;
 
   for (int i=0; i<NUM; i++) {
 	    
@@ -153,18 +200,18 @@ main(int argc, char** argv)
     double s = 0.0, t = 0.0, v = 0.0;
 
     for (int n=0; n<nmax; n++) {
-      s += ans2[n]*ortho->get_dens (x, 0, 0, n, 0);
-      t += ans2[n]*ortho->get_pot  (x, 0, 0, n, 0);
-      v += ans2[n]*ortho->get_force(x, 0, 0, n, 0);
+      s += ans2[n]*ortho->get_dens (x, kx, 0, n, 0);
+      t += ans2[n]*ortho->get_pot  (x, kx, 0, n, 0);
+      v += ans2[n]*ortho->get_force(x, kx, 0, n, 0);
     }
 
     std::cout << std::setw(16) << z
 	      << std::setw(16) << s/(4.0*M_PI)
-	      << std::setw(16) << sech2.get_density(z)
+	      << std::setw(16) << mdl->get_density(z)
 	      << std::setw(16) << t
-	      << std::setw(16) << sech2.get_pot(z)
+	      << std::setw(16) << mdl->get_pot(z)
 	      << std::setw(16) << v
-	      << std::setw(16) << sech2.get_dpot(z)
+	      << std::setw(16) << mdl->get_dpot(z)
 	      << std::endl;
   }
 
@@ -172,20 +219,20 @@ main(int argc, char** argv)
   if (out) {
     
     int NUM = 200;
-    double zmin = -3.0*H, zmax = 3.0*H;
-    double dz = (zmax - zmin)/(NUM-1);
+    double Zmin = -zmax, Zmax = zmax;
+    double dz = (Zmax - Zmin)/(NUM-1);
 
     for (int i=0; i<NUM; i++) {
 	    
-      double z = zmin + dz*i;
+      double z = Zmin + dz*i;
       double x = ortho->z_to_xi(z);
       double s = 0.0;
 
       out << std::setw(16) << z;
       for (int n=0; n<nmax; n++) {
-	out << std::setw(16) << ortho->get_pot  (z, 0, 0, n);
-	out << std::setw(16) << ortho->get_dens (z, 0, 0, n);
-	out << std::setw(16) << ortho->get_force(z, 0, 0, n);
+	out << std::setw(16) << ortho->get_pot  (z, kx, 0, n);
+	out << std::setw(16) << ortho->get_dens (z, kx, 0, n);
+	out << std::setw(16) << ortho->get_force(z, kx, 0, n);
       }
       out << std::endl;
     }
@@ -194,19 +241,25 @@ main(int argc, char** argv)
   }
 
   out.close();
-  out.open(filename + ".ortho");
-  if (out) {
-    auto test = ortho->orthoCheck();
-    int cnt = 0;
-    for ( auto & v : test) {
-      out << "==== " << cnt++ << std::endl << v << std::endl;
+
+  if (vm.count("ortho")) {
+    std::cout << "---- Computing orthogonality matrix\n";
+
+    out.open(filename + ".ortho");
+    if (out) {
+      auto test = ortho->orthoCheck();
+      int cnt = 0;
+      for ( auto & v : test) {
+	out << "==== " << cnt++ << std::endl << v << std::endl;
+      }
+      
+      std::cout << "---- Orthogonality matrix written to <" << filename << ".ortho>\n";
+
+    } else {
+      throw std::runtime_error("Error opening filename <" + filename + ".ortho>");
     }
-    
-  } else {
-    throw std::runtime_error("Error opening filename <" + filename + ".ortho>");
   }
-
-
+  
   return 0;
 }
 
